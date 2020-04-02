6e9e9d267693d5c633b8a5e5e6d68874cd060eb6
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-02 18:16:24 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="movement",
+    version="0.0.1a",
+    python_requires=">=3.6",
+    url="https://github.com/adamltyson/movement",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+)
