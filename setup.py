061331da95db4a52e67552db2838a4bf25e0a146
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-03 15:34:39 +0100

    init structure

--- setup.py
+++ setup.py
@@ -1,11 +1,41 @@
 from setuptools import setup, find_namespace_packages
 
+requirements = ["imlib"]
 
 setup(
     name="movement",
     version="0.0.1a",
+    description="Movement analysis",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "sphinx",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "pydoc-markdown",
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coveralls",
+            "coverage<=4.5.4",
+        ]
+    },
     python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=("docs", "tests*")),
+    include_package_data=True,
     url="https://github.com/adamltyson/movement",
     author="Adam Tyson",
     author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
 )
