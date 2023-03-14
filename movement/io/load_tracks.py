a57575a0d979160faf5331034502d24957a59927
niksirbi <niko.sirbiladze@gmail.com>
2023-03-14 17:51:35 +0000

    DLC picle file holds metadata

--- movement/io/load_tracks.py
+++ movement/io/load_tracks.py
@@ -9,7 +9,7 @@ from movement.utils import validate_dataframe, validate_file_path
 
 # TODO:
 #  - store tracks in a custom Trajectory class instead of DataFrame
-#  - add support for other file formats (e.g. .csv, .pickle)
+#  - add support for other file formats (e.g. .csv)
 
 
 def from_dlc_h5(
