4285ef7e5ea1ffc3261a54ce39b2c4d41f9330c4
niksirbi <niko.sirbiladze@gmail.com>
2023-08-15 17:10:24 +0100

    set xarray.keep_attrs True globally

--- movement/__init__.py
+++ movement/__init__.py
@@ -7,6 +7,10 @@ except PackageNotFoundError:
     # package is not installed
     pass
 
+# set xarray global options
+import xarray as xr
+
+xr.set_options(keep_attrs=True, display_expand_data=False)
 
 # initialize logger upon import
 configure_logging()
