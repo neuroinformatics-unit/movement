72834dd61d7e24b50664fc4e73cb6e985d992434
niksirbi <niko.sirbiladze@gmail.com>
2023-11-15 10:28:24 +0000

    harmonize docstrings, comments,  argument names across save_poses.py

--- movement/io/save_poses.py
+++ movement/io/save_poses.py
@@ -12,16 +12,20 @@ from movement.logging import log_error
 logger = logging.getLogger(__name__)
 
 
-def _xarry_to_dlc_df(ds: xr.Dataset, columns: pd.MultiIndex) -> pd.DataFrame:
+def _xarray_to_dlc_df(ds: xr.Dataset, columns: pd.MultiIndex) -> pd.DataFrame:
     """Takes an xarray dataset and DLC-style multi-index columns and outputs
     a pandas dataframe.
 
     Parameters
     ----------
-    ds : xarray Dataset
+    ds : xarray.Dataset
         Dataset containing pose tracks, confidence scores, and metadata.
-    columns : pd.MultiIndex
+    columns : pandas.MultiIndex
         DLC-style multi-index columns
+
+    Returns
+    -------
+    pandas.DataFrame
     """
 
     # Concatenate the pose tracks and confidence scores into one array
@@ -44,37 +48,30 @@ def _xarry_to_dlc_df(ds: xr.Dataset, columns: pd.MultiIndex) -> pd.DataFrame:
     return df
 
 
-def _auto_split_individuals(ds: xr.Dataset):
+def _auto_split_individuals(ds: xr.Dataset) -> bool:
     """Returns True if there is only one individual in the dataset,
-    else returns False.
-
-    Parameters
-    ----------
-    ds : xarray Dataset
-        Dataset containing pose tracks, confidence scores, and metadata.
-    """
+    else returns False."""
 
     individuals = ds.coords["individuals"].data.tolist()
     return True if len(individuals) == 1 else False
 
 
-def _save_dlc_df(filepath: Path, dataframe: pd.DataFrame):
+def _save_dlc_df(filepath: Path, df: pd.DataFrame) -> None:
     """Given a filepath, will save the dataframe as either a .h5 or .csv.
 
     Parameters
     ----------
-    suffix : os.PathLike
-        Suffix of path provided.
-    filepath : os.PathLike
-        Path of where to save data.
-    suffix : pd.DataFrame
-        Pandas Dataframe to save to .csv or .h5.
+    filepath : pathlib.Path
+        Path of the file to save the dataframe to. The file extension
+        must be either .h5 (recommended) or .csv.
+    df : pandas.DataFrame
+        Pandas Dataframe to save
     """
 
     if filepath.suffix == ".csv":
-        dataframe.to_csv(filepath, sep=",")
+        df.to_csv(filepath, sep=",")
     elif filepath.suffix == ".h5":
-        dataframe.to_hdf(filepath, key="df_with_missing")
+        df.to_hdf(filepath, key="df_with_missing")
     # for invalid suffix
     else:
         raise log_error(ValueError, "Expected filepath to end in .csv or .h5.")
@@ -83,24 +80,23 @@ def _save_dlc_df(filepath: Path, dataframe: pd.DataFrame):
 def to_dlc_df(
     ds: xr.Dataset, split_individuals: bool = True
 ) -> Union[pd.DataFrame, dict[str, pd.DataFrame]]:
-    """Convert an xarray dataset containing pose tracks into a DeepLabCut-style
-    pandas DataFrame with multi-index columns for each individual or a
-    dictionary of DataFrames for each individual based on the
-    'split_individuals' argument.
+    """Convert an xarray dataset containing pose tracks into a single
+    DeepLabCut-style pandas DataFrame or a dictionary of DataFrames
+    per individual, depending on the 'split_individuals' argument.
 
     Parameters
     ----------
-    ds : xarray Dataset
+    ds : xarray.Dataset
         Dataset containing pose tracks, confidence scores, and metadata.
     split_individuals : bool, optional
-        If True, return a dictionary of pandas DataFrames, for each individual.
-        If False, return a single pandas DataFrame with multi-index columns
-        for all individuals.
+        If True, return a dictionary of pandas DataFrames per individual,
+        with individual names as keys and DataFrames as values.
+        If False, return a single pandas DataFrame for all individuals.
         Default is True.
 
     Returns
     -------
-    pandas DataFrame or dict
+    pandas.DataFrame or dict
         DeepLabCut-style pandas DataFrame or dictionary of DataFrames.
 
     Notes
@@ -109,6 +105,7 @@ def to_dlc_df(
     "scorer", "bodyparts", "coords" (if split_individuals is True),
     or "scorer", "individuals", "bodyparts", "coords"
     (if split_individuals is False).
+
     Regardless of the provenance of the points-wise confidence scores,
     they will be referred to as "likelihood", and stored in
     the "coords" level (as DeepLabCut expects).
@@ -116,7 +113,7 @@ def to_dlc_df(
     See Also
     --------
     to_dlc_file : Save the xarray dataset containing pose tracks directly
-        to a DeepLabCut-style ".h5" or ".csv" file.
+        to a DeepLabCut-style .h5 or .csv file.
     """
     if not isinstance(ds, xr.Dataset):
         raise log_error(
@@ -131,41 +128,34 @@ def to_dlc_df(
     individuals = ds.coords["individuals"].data.tolist()
 
     if split_individuals:
-        result = {}
+        df_dict = {}
 
         for individual in individuals:
-            # Select data for the current individual
             individual_data = ds.sel(individuals=individual)
 
-            # Create the DLC-style multi-index columns
             index_levels = ["scorer", "bodyparts", "coords"]
             columns = pd.MultiIndex.from_product(
                 [scorer, bodyparts, coords], names=index_levels
             )
 
-            # Uses the columns and data to make a df
-            df = _xarry_to_dlc_df(individual_data, columns)
-
-            """ Add the DataFrame to the result
-            dictionary with individual's name as key """
-            result[individual] = df
+            df = _xarray_to_dlc_df(individual_data, columns)
+            df_dict[individual] = df
 
         logger.info(
-            """Converted PoseTracks dataset to
-            DLC-style DataFrames for each individual."""
+            "Converted PoseTracks dataset to DeepLabCut-style DataFrames "
+            "per individual."
         )
-        return result
+        return df_dict
     else:
-        # Create the DLC-style multi-index columns
         index_levels = ["scorer", "individuals", "bodyparts", "coords"]
         columns = pd.MultiIndex.from_product(
             [scorer, individuals, bodyparts, coords], names=index_levels
         )
 
-        df = _xarry_to_dlc_df(ds, columns)
+        df_all = _xarray_to_dlc_df(ds, columns)
 
         logger.info("Converted PoseTracks dataset to DLC-style DataFrame.")
-        return df
+        return df_all
 
 
 def to_dlc_file(
@@ -174,34 +164,34 @@ def to_dlc_file(
     split_individuals: Union[bool, Literal["auto"]] = "auto",
 ) -> None:
     """Save the xarray dataset containing pose tracks to a
-    DeepLabCut-style ".h5" or ".csv" file.
+    DeepLabCut-style .h5 or .csv file.
 
     Parameters
     ----------
-    ds : xarray Dataset
+    ds : xarray.Dataset
         Dataset containing pose tracks, confidence scores, and metadata.
-    file_path : pathlib Path or str
+    file_path : pathlib.Path or str
         Path to the file to save the DLC poses to. The file extension
-        must be either ".h5" (recommended) or ".csv".
+        must be either .h5 (recommended) or .csv.
     split_individuals : bool, optional
-        If True, the file will be formatted as in a single-animal
-        DeepLabCut project: no "individuals" level, and each individual will be
-        saved in a separate file. The individual's name will be appended to the
-        file path, just before the file extension, i.e.
+        If True, each individual will be saved to a separate file,
+        formatted as in a single-animal DeepLabCut project - i.e. without
+        the "individuals" column level. The individual's name will be appended
+        to the file path, just before the file extension, i.e.
         "/path/to/filename_individual1.h5".
-        If False, the file will be formatted as in a multi-animal
-        DeepLabCut project: the columns will include the
-        "individuals" level and all individuals will be saved to the same file.
-        If "auto" the format will be determined based on the number of
+        If False, all individuals will be saved to the same file,
+        formatted as in a multi-animal DeepLabCut project - i.e. the columns
+        will include the "individuals" level. The file path will not be
+        modified.
+        If "auto" the argument's value be determined based on the number of
         individuals in the dataset: True if there is only one, and
         False if there are more than one. This is the default.
 
     See Also
     --------
-    to_dlc_df : Convert an xarray dataset containing pose tracks into a
-        DeepLabCut-style pandas DataFrame with multi-index columns
-        for each individual or a dictionary of DataFrames for each individual
-        based on the 'split_individuals' argument.
+    to_dlc_df : Convert an xarray dataset containing pose tracks into a single
+        DeepLabCut-style pandas DataFrame or a dictionary of DataFrames
+        per individual.
 
     Examples
     --------
@@ -227,29 +217,25 @@ def to_dlc_file(
     elif not isinstance(split_individuals, bool):
         raise log_error(
             ValueError,
-            f"Expected 'split_individuals' to be a boolean or 'auto', but got "
+            "Expected 'split_individuals' to be a boolean or 'auto', but got "
             f"{type(split_individuals)}.",
         )
 
     if split_individuals:
-        """If split_individuals is True then it will split the file into a
-        dictionary of pandas dataframes for each individual."""
-
+        # split the dataset into a dictionary of dataframes per individual
         df_dict = to_dlc_df(ds, split_individuals=True)
 
         for key, df in df_dict.items():
-            """Iterates over dictionary, the key is the name of the
-            individual and the value is the corresponding df."""
+            # the key is the individual's name
             filepath = f"{file.path.with_suffix('')}_{key}{file.path.suffix}"
-
             if isinstance(df, pd.DataFrame):
                 _save_dlc_df(Path(filepath), df)
-
-            logger.info(f"Saved PoseTracks dataset to {file.path}.")
+            logger.info(
+                f"Saved PoseTracks data for individual {key} to {file.path}."
+            )
     else:
-        """If split_individuals is False then it will save the file as
-        a dataframe with multi-index columns for each individual."""
-        dataframe = to_dlc_df(ds, split_individuals=False)
-        if isinstance(dataframe, pd.DataFrame):
-            _save_dlc_df(file.path, dataframe)
+        # convert the dataset to a single dataframe for all individuals
+        df_all = to_dlc_df(ds, split_individuals=False)
+        if isinstance(df_all, pd.DataFrame):
+            _save_dlc_df(file.path, df_all)
         logger.info(f"Saved PoseTracks dataset to {file.path}.")
