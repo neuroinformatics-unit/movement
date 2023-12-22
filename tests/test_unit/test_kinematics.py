6121be2ee97d4b740c09cfb1727797b960e432d0
lochhh <changhuan.lo@ucl.ac.uk>
2023-12-22 22:00:23 +0000

    Add test for displacement

--- tests/test_unit/test_kinematics.py
+++ tests/test_unit/test_kinematics.py
@@ -7,6 +7,16 @@ from movement.analysis import kinematics
 class TestKinematics:
     """Test suite for the kinematics module."""
 
+    def test_compute_displacement(self, valid_pose_dataset):
+        """Test the `approximate_derivative` function for
+        calculating displacement."""
+        # Select a single keypoint from a single individual
+        data = valid_pose_dataset.pose_tracks.isel(keypoints=0, individuals=0)
+        # Compute displacement
+        displacement = kinematics.approximate_derivative(data)
+        expected = np.pad([5.0] * 9, (1, 0), "constant")
+        assert np.allclose(displacement, expected)
+
     @pytest.mark.parametrize("method", ["euclidean", "numerical"])
     def test_compute_velocity(self, valid_pose_dataset, method):
         """Test the `compute_velocity` function."""
