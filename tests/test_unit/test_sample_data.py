08b6b349548ea5f54b76b432a556f8daa5480fe3
niksirbi <niko.sirbiladze@gmail.com>
2024-01-08 10:32:05 +0000

    refactored test_sample_data using a fixture

--- tests/test_unit/test_sample_data.py
+++ tests/test_unit/test_sample_data.py
@@ -6,29 +6,29 @@ from xarray import Dataset
 from movement.sample_data import fetch_sample_data, list_sample_data
 
 
-def test_list_sample_data():
-    test_files = [
-        "SLEAP_single-mouse_EPM.analysis.h5",
-        "DLC_single-wasp.predictions.h5",
-        "LP_mouse-face_AIND.predictions.csv",
-    ]
+@pytest.fixture(scope="module")
+def valid_file_names_with_fps():
+    """Return a dict containing one valid file name and the corresponding fps
+    for each supported pose estimation tool."""
+    return {
+        "SLEAP_single-mouse_EPM.analysis.h5": 30,
+        "DLC_single-wasp.predictions.h5": 40,
+        "LP_mouse-face_AIND.predictions.csv": 60,
+    }
 
+
+def test_list_sample_data(valid_file_names_with_fps):
     assert isinstance(list_sample_data(), list)
-    assert all(file in list_sample_data() for file in test_files)
+    assert all(
+        file in list_sample_data() for file in valid_file_names_with_fps
+    )
 
 
-def test_fetch_sample_data():
-    # SLEAP
-    ds_sleap = fetch_sample_data("SLEAP_single-mouse_EPM.analysis.h5")
-    assert isinstance(ds_sleap, Dataset) and ds_sleap.fps == 30
-
-    # DeepLabCut
-    ds_dlc = fetch_sample_data("DLC_single-wasp.predictions.h5")
-    assert isinstance(ds_dlc, Dataset) and ds_dlc.fps == 40
-
-    # LightningPose
-    ds_lp = fetch_sample_data("LP_mouse-face_AIND.predictions.csv")
-    assert isinstance(ds_lp, Dataset) and ds_lp.fps == 60
+def test_fetch_sample_data(valid_file_names_with_fps):
+    # test with valid files
+    for file, fps in valid_file_names_with_fps.items():
+        ds = fetch_sample_data(file)
+        assert isinstance(ds, Dataset) and ds.fps == fps
 
     # Test with an invalid file
     with pytest.raises(ValueError):
