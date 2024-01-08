aca1a42fad1c8136492dabbcc3ee5c0a4534563d
lochhh <changhuan.lo@ucl.ac.uk>
2024-01-08 17:59:26 +0000

    Refactor kinematics test and functions

--- movement/analysis/kinematics.py
+++ movement/analysis/kinematics.py
@@ -2,10 +2,50 @@ import numpy as np
 import xarray as xr
 
 
-def compute_velocity(
-    data: xr.DataArray, method: str = "euclidean"
-) -> np.ndarray:
-    """Compute the instantaneous velocity of a single keypoint from
+def displacement(data: xr.DataArray) -> np.ndarray:
+    """Compute the displacement between consecutive locations
+    of a single keypoint from a single individual.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data, assumed to be of shape (..., 2), where the last
+        dimension contains the x and y coordinates.
+
+    Returns
+    -------
+    numpy.ndarray
+        A numpy array containing the computed magnitude and
+        direction of the displacement.
+    """
+    displacement_vector = np.diff(data, axis=0, prepend=data[0:1])
+    magnitude = np.linalg.norm(displacement_vector, axis=1)
+    direction = np.arctan2(
+        displacement_vector[..., 1], displacement_vector[..., 0]
+    )
+    return np.stack((magnitude, direction), axis=1)
+
+
+def distance(data: xr.DataArray) -> np.ndarray:
+    """Compute the distances between consecutive locations of
+    a single keypoint from a single individual.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data, assumed to be of shape (..., 2), where the last
+        dimension contains the x and y coordinates.
+
+    Returns
+    -------
+    numpy.ndarray
+        A numpy array containing the computed distance.
+    """
+    return displacement(data)[:, 0]
+
+
+def velocity(data: xr.DataArray) -> np.ndarray:
+    """Compute the velocity of a single keypoint from
     a single individual.
 
     Parameters
@@ -13,24 +53,19 @@ def compute_velocity(
     data : xarray.DataArray
         The input data, assumed to be of shape (..., 2), where the last
         dimension contains the x and y coordinates.
-    method : str
-        The method to use for computing velocity. Can be "euclidean" or
-        "numerical".
 
     Returns
     -------
     numpy.ndarray
         A numpy array containing the computed velocity.
     """
-    if method == "euclidean":
-        return compute_euclidean_velocity(data)
     return approximate_derivative(data, order=1)
 
 
-def compute_euclidean_velocity(data: xr.DataArray) -> np.ndarray:
+def speed(data: xr.DataArray) -> np.ndarray:
     """Compute velocity based on the Euclidean norm (magnitude) of the
     differences between consecutive points, i.e. the straight-line
-    distance travelled.
+    distance travelled, assuming equidistant time spacing.
 
     Parameters
     ----------
@@ -43,17 +78,30 @@ def compute_euclidean_velocity(data: xr.DataArray) -> np.ndarray:
     numpy.ndarray
         A numpy array containing the computed velocity.
     """
-    time_diff = data["time"].diff(dim="time")
-    space_diff = np.linalg.norm(np.diff(data.values, axis=0), axis=1)
-    velocity = space_diff / time_diff
-    # Pad with zero to match the original shape of the data
-    velocity = np.concatenate([np.zeros((1,) + velocity.shape[1:]), velocity])
-    return velocity
+    return velocity(data)[:, 0]
 
 
-def approximate_derivative(data: xr.DataArray, order: int = 0) -> np.ndarray:
-    """Compute displacement, velocity, or acceleration using numerical
-    differentiation, assuming equidistant time spacing.
+def acceleration(data: xr.DataArray) -> np.ndarray:
+    """Compute the acceleration of a single keypoint from
+    a single individual.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data, assumed to be of shape (..., 2), where the last
+        dimension contains the x and y coordinates.
+
+    Returns
+    -------
+    numpy.ndarray
+        A numpy array containing the computed acceleration.
+    """
+    return approximate_derivative(data, order=2)
+
+
+def approximate_derivative(data: xr.DataArray, order: int = 1) -> np.ndarray:
+    """Compute velocity or acceleration using numerical differentiation,
+    assuming equidistant time spacing.
 
     Parameters
     ----------
@@ -61,28 +109,27 @@ def approximate_derivative(data: xr.DataArray, order: int = 0) -> np.ndarray:
         The input data, assumed to be of shape (..., 2), where the last
         dimension contains the x and y coordinates.
     order : int
-        The order of the derivative. 0 for displacement, 1 for velocity, 2 for
-        acceleration.
+        The order of the derivative. 1 for velocity, 2 for
+        acceleration. Default is 1.
 
     Returns
     -------
     numpy.ndarray
-        A numpy array containing the computed kinematic variable.
+        A numpy array containing the computed magnitudes and directions of
+        the kinematic variable.
     """
-    if order == 0:  # Compute displacement
-        result = np.diff(data, axis=0)
-        # Pad with zeros to match the original shape of the data
-        result = np.concatenate([np.zeros((1,) + result.shape[1:]), result])
+    if order <= 0:
+        raise ValueError("order must be a positive integer.")
     else:
         result = data
         dt = data["time"].diff(dim="time").values[0]
         for _ in range(order):
             result = np.gradient(result, dt, axis=0)
-        # Pad with zeros to match the output of compute_euclidean_velocity
+        # Prepend with zeros to match match output to the input shape
         result = np.pad(result[1:], ((1, 0), (0, 0)), "constant")
     magnitude = np.linalg.norm(result, axis=-1)
-    # direction = np.arctan2(result[..., 1], result[..., 0])
-    return magnitude
+    direction = np.arctan2(result[..., 1], result[..., 0])
+    return np.stack((magnitude, direction), axis=1)
 
 
 # Locomotion Features
