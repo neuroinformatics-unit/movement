929737be47c4b6897cab506178b4226a19f71c6a
lochhh <changhuan.lo@ucl.ac.uk>
2024-01-26 18:27:06 +0000

    Return x y components in kinematic functions

--- movement/analysis/kinematics.py
+++ movement/analysis/kinematics.py
@@ -2,29 +2,8 @@ import numpy as np
 import xarray as xr
 
 
-def displacement(data: xr.DataArray) -> xr.Dataset:
-    """Compute the displacement between consecutive locations
-    of each keypoint of each individual.
-
-    Parameters
-    ----------
-    data : xarray.DataArray
-        The input data, assumed to be of shape (..., 2), where
-        the last dimension contains the x and y coordinates.
-
-    Returns
-    -------
-    xarray.Dataset
-        An xarray Dataset containing the computed magnitude and
-        direction of displacement.
-    """
-    displacement_xy = data.diff(dim="time")
-    displacement_xy = displacement_xy.reindex(data.coords, fill_value=0)
-    return compute_vector_magnitude_direction(displacement_xy)
-
-
-def distance(data: xr.DataArray) -> xr.DataArray:
-    """Compute the Euclidean distances between consecutive
+def displacement(data: xr.DataArray) -> xr.DataArray:
+    """Compute the displacement between consecutive x, y
     locations of each keypoint of each individual.
 
     Parameters
@@ -36,34 +15,35 @@ def distance(data: xr.DataArray) -> xr.DataArray:
     Returns
     -------
     xarray.DataArray
-        An xarray DataArray containing the magnitude of displacement.
+        An xarray DataArray containing the computed displacement.
     """
-    return displacement(data).magnitude
+    displacement_xy = data.diff(dim="time")
+    displacement_xy = displacement_xy.reindex(data.coords, fill_value=0)
+    return displacement_xy
 
 
-def velocity(data: xr.DataArray) -> xr.Dataset:
-    """Compute the velocity of a single keypoint from
-    a single individual.
+def displacement_vector(data: xr.DataArray) -> xr.Dataset:
+    """Compute the Euclidean magnitude (distance) and direction
+    (angle relative to the positive x-axis) of displacement.
 
     Parameters
     ----------
     data : xarray.DataArray
-        The input data, assumed to be of shape (..., 2), where the last
-        dimension contains the x and y coordinates.
+        The input data, assumed to be of shape (..., 2), where
+        the last dimension contains the x and y coordinates.
 
     Returns
     -------
     xarray.Dataset
-        An xarray Dataset containing the computed magnitude and
-        direction of velocity.
+        An xarray Dataset containing the magnitude and direction
+        of the computed displacement.
     """
-    return approximate_derivative(data, order=1)
+    return compute_vector_magnitude_direction(displacement(data))
 
 
-def speed(data: xr.DataArray) -> xr.DataArray:
-    """Compute speed based on the Euclidean norm (magnitude) of the
-    differences between consecutive points, i.e. the straight-line
-    distance travelled, assuming equidistant time spacing.
+def velocity(data: xr.DataArray) -> xr.DataArray:
+    """Compute the velocity between consecutive x, y locations
+    of each keypoint of each individual.
 
     Parameters
     ----------
@@ -74,14 +54,33 @@ def speed(data: xr.DataArray) -> xr.DataArray:
     Returns
     -------
     xarray.DataArray
-        An xarray DataArray containing the magnitude of velocity.
+        An xarray Dataset containing the computed velocity.
     """
-    return velocity(data).magnitude
+    return approximate_derivative(data, order=1)
 
 
-def acceleration(data: xr.DataArray) -> xr.Dataset:
-    """Compute the acceleration of a single keypoint from
-    a single individual.
+def velocity_vector(data: xr.DataArray) -> xr.Dataset:
+    """Compute the Euclidean magnitude (speed) and direction
+    (angle relative to the positive x-axis) of velocity.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data, assumed to be of shape (..., 2), where
+        the last dimension contains the x and y coordinates.
+
+    Returns
+    -------
+    xarray.Dataset
+        An xarray Dataset containing the magnitude and direction
+        of the computed velocity.
+    """
+    return compute_vector_magnitude_direction(velocity(data))
+
+
+def acceleration(data: xr.DataArray) -> xr.DataArray:
+    """Compute the acceleration between consecutive x, y
+    locations of each keypoint of each individual.
 
     Parameters
     ----------
@@ -98,7 +97,25 @@ def acceleration(data: xr.DataArray) -> xr.Dataset:
     return approximate_derivative(data, order=2)
 
 
-def approximate_derivative(data: xr.DataArray, order: int = 1) -> xr.Dataset:
+def acceleration_vector(data: xr.DataArray) -> xr.Dataset:
+    """Compute the Euclidean magnitude and direction of acceleration.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data, assumed to be of shape (..., 2), where
+        the last dimension contains the x and y coordinates.
+
+    Returns
+    -------
+    xarray.Dataset
+        An xarray Dataset containing the magnitude and direction
+        of the computed acceleration.
+    """
+    return compute_vector_magnitude_direction(acceleration(data))
+
+
+def approximate_derivative(data: xr.DataArray, order: int = 1) -> xr.DataArray:
     """Compute velocity or acceleration using numerical differentiation,
     assuming equidistant time spacing.
 
@@ -113,9 +130,8 @@ def approximate_derivative(data: xr.DataArray, order: int = 1) -> xr.Dataset:
 
     Returns
     -------
-    xarray.Dataset
-        An xarray Dataset containing the computed magnitudes and
-        directions of the derived variable.
+    xarray.DataArray
+        An xarray DataArray containing the derived variable.
     """
     if order <= 0:
         raise ValueError("order must be a positive integer.")
@@ -130,7 +146,7 @@ def approximate_derivative(data: xr.DataArray, order: int = 1) -> xr.Dataset:
                 kwargs={"axis": 0},
             )
         result = result.reindex_like(data)
-    return compute_vector_magnitude_direction(result)
+    return result
 
 
 def compute_vector_magnitude_direction(input: xr.DataArray) -> xr.Dataset:
