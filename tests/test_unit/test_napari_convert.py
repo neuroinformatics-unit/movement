6816e173160e9dc3dc87683e2ce53dbc3f5094de
niksirbi <niko.sirbiladze@gmail.com>
2024-02-27 17:57:31 +0000

    test replacement of NaNs in confidence

--- tests/test_unit/test_napari_convert.py
+++ tests/test_unit/test_napari_convert.py
@@ -2,17 +2,66 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from movement.napari.convert import ds_to_napari_tracks
+from movement.napari.convert import (
+    _replace_nans_with_zeros,
+    ds_to_napari_tracks,
+)
 
 
 class TestNapariConvert:
     """Test suite for the movement.napari.convert module."""
 
-    @pytest.mark.parametrize(
-        "ds_name", ["valid_pose_dataset", "valid_pose_dataset_with_nan"]
-    )
+    @pytest.fixture
+    def confidence_with_some_nan(self, valid_pose_dataset):
+        """Return a valid pose dataset with some NaNs in confidence values."""
+        valid_pose_dataset["confidence"].loc[
+            {"individuals": "ind1", "time": [3, 7, 8]}
+        ] = np.nan
+        return valid_pose_dataset
+
+    @pytest.fixture
+    def confidence_with_all_nan(self, valid_pose_dataset):
+        """Return a valid pose dataset with all NaNs in confidence values."""
+        valid_pose_dataset["confidence"].data = np.full_like(
+            valid_pose_dataset["confidence"].data, np.nan
+        )
+        return valid_pose_dataset
+
+    datasets_to_test = [
+        "valid_pose_dataset",
+        "valid_pose_dataset_with_nan",
+        "confidence_with_some_nan",
+        "confidence_with_all_nan",
+    ]
+
+    @pytest.mark.parametrize("ds_name", datasets_to_test)
+    def test_replace_confidence_nans_with_zeros(
+        self, ds_name, request, caplog
+    ):
+        """Test that NaN confidence values are replaced with zeros.
+        Also checks that a warning is logged when NaNs are found."""
+        ds = request.getfixturevalue(ds_name)
+        nan_coords = (
+            ds["confidence"].where(ds["confidence"].isnull(), drop=True).coords
+        )
+
+        ds_new = _replace_nans_with_zeros(ds, ["confidence"])
+
+        # Warning should be logged if confidence contained NaNs
+        if ds_name in ["confidence_with_some_nan", "confidence_with_all_nan"]:
+            assert "NaNs found in confidence" in caplog.records[0].message
+            assert caplog.records[0].levelname == "WARNING"
+        else:
+            assert len(caplog.records) == 0
+
+        # Check that the NaN values have been replaced with zeros
+        assert np.all(ds_new["confidence"].notnull())
+        assert np.all(ds_new["confidence"].sel(nan_coords).data == 0)
+
+    @pytest.mark.parametrize("ds_name", datasets_to_test)
     def test_ds_to_napari_tracks(self, ds_name, request):
-        """Test ds_to_napari_tracks."""
+        """Test that the conversion from xarray dataset to napari tracks
+        returns the expected data and properties."""
 
         ds = request.getfixturevalue(ds_name)
         n_frames = ds.sizes["time"]
@@ -41,8 +90,7 @@ class TestNapariConvert:
             (np.tile(base * 4, n_tracks), np.tile(base * base, n_tracks))
         )
         if ds_name == "valid_pose_dataset_with_nan":
-            nan_rows = [3, 7, 8, 13, 17, 18]
-            expected_yx[nan_rows] = np.nan
+            expected_yx[[3, 7, 8, 13, 17, 18]] = np.nan
         np.testing.assert_allclose(data[:, 2:], expected_yx, equal_nan=True)
 
         # Check that the properties dataframe has the expected values
@@ -60,7 +108,14 @@ class TestNapariConvert:
         ]
         assert np.array_equal(props["keypoint"], expected_keypoints)
         np.testing.assert_allclose(props["time"], expected_frame_ids)
-        expected_confidence = np.full(n_frames * n_tracks, 1.0)
-        np.testing.assert_allclose(props["confidence"], expected_confidence)
+
         # Make sure that confidence values are finite
         assert np.all(np.isfinite(props["confidence"]))
+        # Check that confidence values are all 1.0
+        # Except where NaNs were replaced with zeros
+        expected_confidence = np.full(n_frames * n_tracks, 1.0)
+        if ds_name == "confidence_with_all_nan":
+            expected_confidence = np.zeros(n_frames * n_tracks)
+        elif ds_name == "confidence_with_some_nan":
+            expected_confidence[[3, 7, 8, 13, 17, 18]] = 0.0
+        np.testing.assert_allclose(props["confidence"], expected_confidence)
