046f092fc808395a9878fbb0bc0bc1587f04c547
lochhh <changhuan.lo@ucl.ac.uk>
2024-03-22 16:52:05 +0000

    Add tests and `pol2cart`

--- movement/analysis/vector_utils.py
+++ movement/analysis/vector_utils.py
@@ -2,13 +2,15 @@ import numpy as np
 import xarray as xr
 
 
-def cart2polar(data: xr.DataArray) -> xr.DataArray:
+def cart2pol(data: xr.DataArray) -> xr.DataArray:
     """Transform Cartesian coordinates to polar.
 
     Parameters
     ----------
     data : xarray.DataArray
-        The input data containing ``space`` as a dimension.
+        The input data containing ``space`` as a dimension with
+        ``x`` and ``y`` labels.
+
     Returns
     -------
     xarray.DataArray
@@ -38,4 +40,36 @@ def cart2polar(data: xr.DataArray) -> xr.DataArray:
     ).transpose(*dims)
 
 
-# validate space dimension
+def pol2cart(data: xr.DataArray) -> xr.DataArray:
+    """Transform polar coordinates to Cartesian.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        The input data containing ``space_polar`` as a dimension with
+        ``rho`` and ``theta`` labels.
+
+    Returns
+    -------
+    xarray.DataArray
+        An xarray DataArray containing the Cartesian coordinates
+        x and y.
+    """
+    rho = data.sel(space_polar="rho")
+    theta = data.sel(space_polar="theta")
+    x = rho * np.cos(theta)
+    y = rho * np.sin(theta)
+    # Replace space_polar dim with space
+    dims = list(data.dims)
+    dims[dims.index("space_polar")] = "space"
+    return xr.combine_nested(
+        [
+            x.assign_coords({"space": "x"}),
+            y.assign_coords({"space": "y"}),
+        ],
+        concat_dim="space",
+    ).transpose(*dims)
+
+
+# validate space dimension with x and y
+# validate space_polar dimension with rho and theta
--- tests/test_unit/test_vector_utils.py
+++ tests/test_unit/test_vector_utils.py
@@ -13,78 +13,110 @@ class TestVectorUtils:
         """Return a function to generate the expected dataset
         for different kinematic properties."""
 
-        def _expected_dataset(name):
-            """Return an xarray.Dataset with expected norm and
-            theta values."""
-            dims = valid_pose_dataset.pose_tracks.dims[:-1]
+        def _expected_dataset(input_type=None):
+            """Return an xarray.Dataset with input and expected output."""
+            x_vals = [1, 1, 0, -1, -10, -1, 0, 1, 5, 3.5355, 0, -2]
+            y_vals = [0, 1, 1, 1, 0, -1, -1, -1, 0, 3.5355, 10, -2]
+            time_coords = np.arange(len(x_vals))
+            expected_theta = np.pi * np.array(
+                [
+                    0,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1,
+                    -0.75,
+                    -0.5,
+                    -0.25,
+                    0,
+                    0.25,
+                    0.5,
+                    -0.75,
+                ]
+            )
+            expected_rho = np.sqrt(
+                np.array(x_vals) ** 2 + np.array(y_vals) ** 2
+            )
+            if input_type == "multi_dim_input":
+                n_individuals = valid_pose_dataset.coords["individuals"].size
+                n_keypoints = valid_pose_dataset.coords["keypoints"].size
+                n_frames = int(len(x_vals) / (n_individuals * n_keypoints))
+                input_data = xr.DataArray(
+                    np.column_stack((x_vals, y_vals)).reshape(
+                        n_frames, n_individuals, n_keypoints, 2
+                    ),
+                    coords={
+                        "time": np.arange(n_frames),
+                        "individuals": valid_pose_dataset.coords[
+                            "individuals"
+                        ],
+                        "keypoints": valid_pose_dataset.coords["keypoints"],
+                        "space": valid_pose_dataset.coords["space"],
+                    },
+                )
+                expected = xr.DataArray(
+                    np.column_stack((expected_rho, expected_theta)).reshape(
+                        n_frames, n_individuals, n_keypoints, 2
+                    ),
+                    coords={
+                        "time": np.arange(n_frames),
+                        "individuals": valid_pose_dataset.coords[
+                            "individuals"
+                        ],
+                        "keypoints": valid_pose_dataset.coords["keypoints"],
+                        "space_polar": ["rho", "theta"],
+                    },
+                )
+            else:
+                input_data = xr.DataArray(
+                    np.column_stack((x_vals, y_vals)),
+                    dims=["time", "space"],
+                    coords={"time": time_coords, "space": ["x", "y"]},
+                )
+                expected = xr.DataArray(
+                    np.column_stack((expected_rho, expected_theta)),
+                    dims=["time", "space_polar"],
+                    coords={
+                        "time": time_coords,
+                        "space_polar": ["rho", "theta"],
+                    },
+                )
             ds = xr.Dataset(
                 data_vars={
-                    "rho": xr.DataArray(
-                        np.sqrt(
-                            np.square(
-                                valid_pose_dataset.pose_tracks.sel(space="x")
-                            )
-                            + np.square(
-                                valid_pose_dataset.pose_tracks.sel(space="y")
-                            )
-                        ),
-                        dims=dims,
-                    ),
-                    "theta": xr.DataArray(
-                        np.full((10, 2, 2), 0.92729522),
-                        dims=dims,
-                    ),
-                },
-                coords={
-                    "time": valid_pose_dataset.time,
-                    "individuals": valid_pose_dataset.individuals,
-                    "keypoints": valid_pose_dataset.keypoints,
+                    "input": input_data,
+                    "expected": expected,
                 },
             )
-            if name == "displacement":
-                # Set the first values to zero
-                ds.rho[0, :, :] = 0
-                ds.theta[0, :, :] = 0
-            elif name == "acceleration":
-                # Set all values to zero
-                ds.rho[:] = 0
-                ds.theta[:] = 0
             return ds
 
         return _expected_dataset
 
-    def test_cart2polar_(self):
-        """Test rho computation."""
-        x_vals = [5, 3.5355, 0, -10]
-        y_vals = [0, 3.5355, 10, 0]
-        time_coords = np.arange(len(x_vals), dtype=int)
-        input_data = xr.DataArray(
-            np.column_stack((x_vals, y_vals)),
-            dims=["time", "space"],
-            coords={"time": time_coords, "space": ["x", "y"]},
-        )
-        expected_theta = [0, 0.7854, 1.5708, 3.1416]
-        expected_rho = [5.0000, 5.0000, 10.0000, 10.0000]
-        expected = xr.DataArray(
-            np.column_stack((expected_rho, expected_theta)),
-            dims=["time", "space_polar"],
-            coords={"time": time_coords, "space_polar": ["rho", "theta"]},
-        )
-        result = vector_utils.cart2polar(input_data)
-        xr.testing.assert_allclose(result, expected)
+    @pytest.mark.parametrize(
+        "input_type", ["single_dim_input", "multi_dim_input"]
+    )
+    def test_cart2pol_(self, expected_dataset, input_type):
+        """Test cart2pol with known values."""
+        params = expected_dataset(input_type)
+        result = vector_utils.cart2pol(params.input)
+        xr.testing.assert_allclose(result, params.expected)
 
-    def test_cart2polar(
-        self, valid_pose_dataset, kinematic_property, expected_dataset
-    ):
-        """Test rho computation for different kinematic properties."""
+    @pytest.mark.parametrize(
+        "input_type", ["single_dim_input", "multi_dim_input"]
+    )
+    def test_pol2cart(self, expected_dataset, input_type):
+        """Test cart2pol with known values."""
+        params = expected_dataset(input_type)
+        result = vector_utils.pol2cart(params.expected)
+        xr.testing.assert_allclose(result, params.input)
+
+    def test_cart2pol_pol2cart(self, valid_pose_dataset, kinematic_property):
+        """Test transformation between Cartesian and polar coordinates."""
         data = getattr(valid_pose_dataset.move, kinematic_property)
-        result = vector_utils.cart2polar(data)
-        expected = xr.DataArray(
-            np.sqrt(
-                np.square(data.sel(space="x")) + np.square(data.sel(space="y"))
-            ),
-            dims=data.dims[:-1],
-        )
-        xr.testing.assert_allclose(
-            result.sel(space_polar="rho", drop=True), expected
-        )
+        polar_data = vector_utils.cart2pol(data)
+        cartesian_data = vector_utils.pol2cart(polar_data)
+
+        xr.testing.assert_allclose(cartesian_data, data)
+
+    # TODO: test with missing "space" dimension
+    # TODO: test with missing "x" and "y" variables
+    # TODO: test with Nan values
