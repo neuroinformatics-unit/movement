b32ba0b7783aaa057c4af72db5b334c12f0cca6c
lochhh <changhuan.lo@ucl.ac.uk>
2024-03-25 15:32:16 +0000

    Prefer readabiliity

--- tests/test_unit/test_vector_utils.py
+++ tests/test_unit/test_vector_utils.py
@@ -12,82 +12,105 @@ class TestVectorUtils:
 
     @pytest.fixture
     def cart_pol_dataset(self):
-        def _cart_pol_dataset(ds_type):
-            """Return an xarray.Dataset with Cartesian and polar
-            coordinates, and expected exception depending on the
-            type of requested."""
-            x_vals = np.array([1, 1, 0, -1, -10, -1, 0, 1], dtype=float)
-            y_vals = np.array([0, 1, 1, 1, 0, -1, -1, -1], dtype=float)
-            time_coords = np.arange(len(x_vals))
-            rho = np.sqrt(x_vals**2 + y_vals**2)
-            theta = np.pi * np.array(
-                [0, 0.25, 0.5, 0.75, 1, -0.75, -0.5, -0.25]
-            )
-            cart = xr.DataArray(
-                np.column_stack((x_vals, y_vals)),
-                dims=["time", "space"],
-                coords={"time": time_coords, "space": ["x", "y"]},
-            )
-            pol = xr.DataArray(
-                np.column_stack((rho, theta)),
-                dims=["time", "space_polar"],
-                coords={
-                    "time": time_coords,
-                    "space_polar": ["rho", "theta"],
-                },
-            )
-            ds = xr.Dataset(
-                data_vars={
-                    "cart": cart,
-                    "pol": pol,
-                },
-            )
-            ds.attrs.update({"expected_exception": does_not_raise()})
-            if ds_type == "contains_nan":
-                ds.cart.loc[{"time": slice(2, 3)}] = np.nan
-                ds.pol.loc[{"time": slice(2, 3)}] = np.nan
-            elif ds_type == "missing_cart_dim":
-                ds = ds.rename({"space": "spice"})
-                ds.attrs.update(
-                    {"expected_exception": pytest.raises(ValueError)}
-                )
-            elif ds_type == "missing_cart_coords":
-                ds["space"] = ["a", "b"]
-                ds.attrs.update(
-                    {"expected_exception": pytest.raises(ValueError)}
-                )
-            elif ds_type == "missing_polar_dim":
-                ds = ds.rename({"space_polar": "spice_polar"})
-                ds.attrs.update(
-                    {"expected_exception": pytest.raises(ValueError)}
-                )
-            elif ds_type == "missing_polar_coords":
-                ds["space_polar"] = ["a", "b"]
-                ds.attrs.update(
-                    {"expected_exception": pytest.raises(ValueError)}
-                )
-            return ds
+        """Return an xarray.Dataset with Cartesian and polar coordinates."""
+        x_vals = np.array([1, 1, 0, -1, -10, -1, 0, 1], dtype=float)
+        y_vals = np.array([0, 1, 1, 1, 0, -1, -1, -1], dtype=float)
+        time_coords = np.arange(len(x_vals))
+        rho = np.sqrt(x_vals**2 + y_vals**2)
+        theta = np.pi * np.array([0, 0.25, 0.5, 0.75, 1, -0.75, -0.5, -0.25])
+        cart = xr.DataArray(
+            np.column_stack((x_vals, y_vals)),
+            dims=["time", "space"],
+            coords={"time": time_coords, "space": ["x", "y"]},
+        )
+        pol = xr.DataArray(
+            np.column_stack((rho, theta)),
+            dims=["time", "space_polar"],
+            coords={
+                "time": time_coords,
+                "space_polar": ["rho", "theta"],
+            },
+        )
+        return xr.Dataset(
+            data_vars={
+                "cart": cart,
+                "pol": pol,
+            },
+        )
 
-        return _cart_pol_dataset
+    @pytest.fixture
+    def cart_pol_dataset_with_nan(self, cart_pol_dataset):
+        """Return an xarray.Dataset with Cartesian and polar coordinates,
+        where some values are NaN."""
+        cart_pol_dataset.cart.loc[{"time": slice(2, 3)}] = np.nan
+        cart_pol_dataset.pol.loc[{"time": slice(2, 3)}] = np.nan
+        return cart_pol_dataset
+
+    @pytest.fixture
+    def cart_pol_dataset_missing_cart_dim(self, cart_pol_dataset):
+        """Return an xarray.Dataset with Cartesian and polar coordinates,
+        where the required ``space`` dimension is missing."""
+        return cart_pol_dataset.rename({"space": "spice"})
+
+    @pytest.fixture
+    def cart_pol_dataset_missing_cart_coords(self, cart_pol_dataset):
+        """Return an xarray.Dataset with Cartesian and polar coordinates,
+        where the required ``space["x"]`` and ``space["y"]`` coordinates
+        are missing."""
+        cart_pol_dataset["space"] = ["a", "b"]
+        return cart_pol_dataset
+
+    @pytest.fixture
+    def cart_pol_dataset_missing_pol_dim(self, cart_pol_dataset):
+        """Return an xarray.Dataset with Cartesian and polar coordinates,
+        where the required ``space_polar`` dimension is missing."""
+        return cart_pol_dataset.rename({"space_polar": "spice_polar"})
+
+    @pytest.fixture
+    def cart_pol_dataset_missing_pol_coords(self, cart_pol_dataset):
+        """Return an xarray.Dataset with Cartesian and polar coordinates,
+        where the required ``space_polar["rho"]`` and ``space_polar["theta"]``
+        coordinates are missing."""
+        cart_pol_dataset["space_polar"] = ["a", "b"]
+        return cart_pol_dataset
 
     @pytest.mark.parametrize(
-        "ds_type",
-        [None, "contains_nan", "missing_cart_dim", "missing_cart_coords"],
+        "ds, expected_exception",
+        [
+            ("cart_pol_dataset", does_not_raise()),
+            ("cart_pol_dataset_with_nan", does_not_raise()),
+            ("cart_pol_dataset_missing_cart_dim", pytest.raises(ValueError)),
+            (
+                "cart_pol_dataset_missing_cart_coords",
+                pytest.raises(ValueError),
+            ),
+        ],
     )
-    def test_cart2pol(self, cart_pol_dataset, ds_type):
+    def test_cart2pol(self, ds, expected_exception, request):
         """Test Cartesian to polar coordinates with known values."""
-        ds = cart_pol_dataset(ds_type)
-        with ds.expected_exception:
+        ds = request.getfixturevalue(ds)
+        with expected_exception:
             result = vector_utils.cart2pol(ds.cart)
             xr.testing.assert_allclose(result, ds.pol)
 
     @pytest.mark.parametrize(
-        "ds_type",
-        [None, "contains_nan", "missing_polar_dim", "missing_polar_coords"],
+        "ds, expected_exception",
+        [
+            ("cart_pol_dataset", does_not_raise()),
+            ("cart_pol_dataset_with_nan", does_not_raise()),
+            (
+                "cart_pol_dataset_missing_pol_dim",
+                pytest.raises(ValueError),
+            ),
+            (
+                "cart_pol_dataset_missing_pol_coords",
+                pytest.raises(ValueError),
+            ),
+        ],
     )
-    def test_pol2cart(self, cart_pol_dataset, ds_type):
+    def test_pol2cart(self, ds, expected_exception, request):
         """Test polar to Cartesian coordinates with known values."""
-        ds = cart_pol_dataset(ds_type)
-        with ds.expected_exception:
+        ds = request.getfixturevalue(ds)
+        with expected_exception:
             result = vector_utils.pol2cart(ds.pol)
             xr.testing.assert_allclose(result, ds.cart)
