4e3ae27985c2ca1b912f4340dd2bb7f793a7ed34
lochhh <changhuan.lo@ucl.ac.uk>
2024-03-25 15:54:58 +0000

    Add analysis integration test

---
+++ tests/test_integration/test_analysis.py
@@ -0,0 +1,30 @@
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+import xarray as xr
+
+from movement.analysis import vector_utils
+
+
+class TestAnalysis:
+    """Test suite for the analysis module."""
+
+    @pytest.mark.parametrize(
+        "ds, expected_exception",
+        [
+            ("valid_pose_dataset", does_not_raise()),
+            ("valid_pose_dataset_with_nan", does_not_raise()),
+            ("missing_dim_dataset", pytest.raises(ValueError)),
+        ],
+    )
+    def test_cart_and_pol_transform(
+        self, ds, expected_exception, kinematic_property, request
+    ):
+        """Test transformation between Cartesian and polar coordinates
+        with various kinematic properties."""
+        ds = request.getfixturevalue(ds)
+        with expected_exception:
+            data = getattr(ds.move, kinematic_property)
+            polar_data = vector_utils.cart2pol(data)
+            cartesian_data = vector_utils.pol2cart(polar_data)
+            xr.testing.assert_allclose(cartesian_data, data)
