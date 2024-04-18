fea66746ef3cf7feaca1f324ff2790708ebf69cc
Eric Denovellis <edeno@bu.edu>
2024-04-18 10:21:54 -0500

    NWB requires one file per individual

--- movement/io/nwb_export.py
+++ movement/io/nwb_export.py
@@ -1,4 +1,5 @@
 import xarray as xr
+from typing import Union
 
 try:
     import ndx_pose
@@ -10,6 +11,7 @@ except ImportError:
 
 def _create_pose_and_skeleton_objects(
     ds: xr.Dataset,
+    subject: str,
     pose_estimation_series_kwargs: dict = None,
     pose_estimation_kwargs: dict = None,
     skeleton_kwargs: dict = None,
@@ -20,6 +22,8 @@ def _create_pose_and_skeleton_objects(
     ----------
     ds : xr.Dataset
         Movement dataset containing the data to be converted to NWB.
+    subject : str
+        Name of the subject to be converted.
     pose_estimation_series_kwargs : dict, optional
         PoseEstimationSeries keyword arguments. See ndx_pose, by default None
     pose_estimation_kwargs : dict, optional
@@ -61,53 +65,41 @@ def _create_pose_and_skeleton_objects(
             source_software_version=None,
         )
 
-    skeleton_list = []
-    pose_estimation = []
+    pose_estimation_series = []
 
-    for subject in ds.individuals.to_numpy():
-        pose_estimation_series = []
-
-        for keypoint in ds.keypoints.to_numpy():
-            pose_estimation_series.append(
-                ndx_pose.PoseEstimationSeries(
-                    name=keypoint,
-                    data=ds.sel(
-                        keypoints=keypoint, individuals=subject
-                    ).position.to_numpy(),
-                    confidence=ds.sel(
-                        keypoints=keypoint, individuals=subject
-                    ).confidence.to_numpy(),
-                    unit="pixels",
-                    timestamps=ds.sel(
-                        keypoints=keypoint, individuals=subject
-                    ).time.to_numpy(),
-                    **pose_estimation_series_kwargs,
-                )
-            )
-
-        skeleton_list.append(
-            ndx_pose.Skeleton(
-                name=f"{subject}_skeleton",
-                nodes=ds.sel(individuals=subject).keypoints.to_numpy().tolist(),
-                **skeleton_kwargs,
+    for keypoint in ds.keypoints.to_numpy():
+        pose_estimation_series.append(
+            ndx_pose.PoseEstimationSeries(
+                name=keypoint,
+                data=ds.sel(keypoints=keypoint).position.to_numpy(),
+                confidence=ds.sel(keypoints=keypoint).confidence.to_numpy(),
+                unit="pixels",
+                timestamps=ds.sel(keypoints=keypoint).time.to_numpy(),
+                **pose_estimation_series_kwargs,
             )
         )
 
-        bodyparts_str = ", ".join(
-            ds.sel(individuals=subject).keypoints.to_numpy().tolist()
+    skeleton_list = [
+        ndx_pose.Skeleton(
+            name=f"{subject}_skeleton",
+            nodes=ds.keypoints.to_numpy().tolist(),
+            **skeleton_kwargs,
         )
-        description = f"Estimated positions of {bodyparts_str} of {subject} using {ds.source_software}."
+    ]
 
-        pose_estimation.append(
-            ndx_pose.PoseEstimation(
-                name="PoseEstimation",
-                pose_estimation_series=pose_estimation_series,
-                description=description,
-                source_software=ds.source_software,
-                skeleton=skeleton_list[-1],
-                **pose_estimation_kwargs,
-            )
+    bodyparts_str = ", ".join(ds.keypoints.to_numpy().tolist())
+    description = f"Estimated positions of {bodyparts_str} of {subject} using {ds.source_software}."
+
+    pose_estimation = [
+        ndx_pose.PoseEstimation(
+            name="PoseEstimation",
+            pose_estimation_series=pose_estimation_series,
+            description=description,
+            source_software=ds.source_software,
+            skeleton=skeleton_list[-1],
+            **pose_estimation_kwargs,
         )
+    ]
 
     skeletons = ndx_pose.Skeletons(skeletons=skeleton_list)
 
@@ -115,29 +107,43 @@ def _create_pose_and_skeleton_objects(
 
 
 def convert_movement_to_nwb(
-    nwbfile: pynwb.NWBFile,
+    nwbfiles: Union[list[pynwb.NWBFile], pynwb.NWBFile],
     ds: xr.Dataset,
     pose_estimation_series_kwargs: dict = None,
     pose_estimation_kwargs: dict = None,
     skeletons_kwargs: dict = None,
 ):
-    pose_estimation, skeletons = _create_pose_and_skeleton_objects(
-        ds, pose_estimation_series_kwargs, pose_estimation_kwargs, skeletons_kwargs
-    )
-    try:
-        behavior_pm = nwbfile.create_processing_module(
-            name="behavior",
-            description="processed behavioral data",
-        )
-    except ValueError:
-        print("Behavior processing module already exists. Skipping...")
-        behavior_pm = nwbfile.processing["behavior"]
+    if isinstance(nwbfiles, pynwb.NWBFile):
+        nwbfiles = [nwbfiles]
 
-    try:
-        behavior_pm.add(skeletons)
-    except ValueError:
-        print("Skeletons already exists. Skipping...")
-    try:
-        behavior_pm.add(pose_estimation)
-    except ValueError:
-        print("PoseEstimation already exists. Skipping...")
+    if len(nwbfiles) != len(ds.individuals):
+        raise ValueError(
+            "Number of NWBFiles must be equal to the number of individuals in the dataset. "
+            "NWB requires one file per individual."
+        )
+
+    for nwbfile, subject in zip(nwbfiles, ds.individuals.to_numpy()):
+        pose_estimation, skeletons = _create_pose_and_skeleton_objects(
+            ds.sel(individuals=subject),
+            subject,
+            pose_estimation_series_kwargs,
+            pose_estimation_kwargs,
+            skeletons_kwargs,
+        )
+        try:
+            behavior_pm = nwbfile.create_processing_module(
+                name="behavior",
+                description="processed behavioral data",
+            )
+        except ValueError:
+            print("Behavior processing module already exists. Skipping...")
+            behavior_pm = nwbfile.processing["behavior"]
+
+        try:
+            behavior_pm.add(skeletons)
+        except ValueError:
+            print("Skeletons already exists. Skipping...")
+        try:
+            behavior_pm.add(pose_estimation)
+        except ValueError:
+            print("PoseEstimation already exists. Skipping...")
