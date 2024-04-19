62c579470f653358d77587eff16eabc283ffa227
Eric Denovellis <edeno@bu.edu>
2024-04-19 10:03:36 -0500

    Fix linting based on pre-commit hooks

--- examples/nwb_conversion.py
+++ examples/nwb_conversion.py
@@ -1,22 +1,26 @@
-from movement import sample_data
-from pynwb import NWBFile
 import datetime
+
+from pynwb import NWBFile
+
+from movement import sample_data
 from movement.io.nwb_export import convert_movement_to_nwb
 
 # Load the sample data
 ds = sample_data.fetch_sample_data("DLC_two-mice.predictions.csv")
 
-# The dataset has two individuals, we will create two NWBFiles for each individual
+# The dataset has two individuals
+# we will create two NWBFiles for each individual
 
+session_start_time = datetime.datetime.now(datetime.timezone.utc)
 nwbfile_individual1 = NWBFile(
     session_description="session_description",
     identifier="individual1",
-    session_start_time=datetime.datetime.now(datetime.timezone.utc),
+    session_start_time=session_start_time,
 )
 nwbfile_individual2 = NWBFile(
     session_description="session_description",
     identifier="individual2",
-    session_start_time=datetime.datetime.now(datetime.timezone.utc),
+    session_start_time=session_start_time,
 )
 
 nwbfiles = [nwbfile_individual1, nwbfile_individual2]
