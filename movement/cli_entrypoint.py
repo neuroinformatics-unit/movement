798068853e29384ae85c168f1fd141e88d25be39
Chang Huan Lo <changhuan.lo@ucl.ac.uk>
2024-05-13 19:58:10 +0100

    Add CLI entrypoint (#176)
    * Add CLI entrypoint
    
    * Use argparse
    
    * Update comments
    
    * Add instructions to verify installation
    
    * Rename subparser title
    
    * Add system to info output
    
    * Revert "Add system to info output"
    
    This reverts commit 8e907193c6958f0527512f038e64d7a5505dd2bb.

---
+++ movement/cli_entrypoint.py
@@ -0,0 +1,72 @@
+"""CLI entrypoint for the ``movement`` package."""
+
+import argparse
+import platform
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import movement
+
+ASCII_ART = """
+      _ __ ___   _____   _____ _ __ ___   ___ _ __ | |_
+     | '_ ` _ \ / _ \ \ / / _ \ '_ ` _ \ / _ \ '_ \| __|
+     | | | | | | (_) \ V /  __/ | | | | |  __/ | | | |_
+     |_| |_| |_|\___/ \_/ \___|_| |_| |_|\___|_| |_|\__|
+
+                       .******
+                   ,******/*******
+               ,*******/******(###  **
+           ,/*************(###((**/****/**
+       ,*/******/*****(####(***/***/**/***(#.
+     .*******/****((###(****/**/***/**(#####//////
+     ...*.****(####(****/**/****/*(#####//////////////
+     .,.*...###(*****/*/****/*(#####///////////////###
+     ...*.,.#(#,,,/******/(##(##///////////////#######
+     ..**...###,,,*,,,(#####///////////////###########
+     ...*..*(##,,,*,,,###.////////////(###############
+     .../...###/,,*,,,###...,.////(###################
+     ...*...(##,,,*/,,###.,.,...####################(#
+         ...###,*,*,,,###...,...################(#####
+               ,,,*,*,##(..*,...############(######(
+                 ,*,,,###...,..*########(######(
+                     ,#/ .../...####(######(
+                         ,..,...(######(
+                             ,..###(
+    """
+
+
+def main() -> None:
+    """Entrypoint for the CLI."""
+    parser = argparse.ArgumentParser(prog="movement")
+    subparsers = parser.add_subparsers(dest="command", title="commands")
+    # Add 'info' command
+    info_parser = subparsers.add_parser(
+        "info", help="output diagnostic information about the environment"
+    )
+    info_parser.set_defaults(func=info)
+
+    args = parser.parse_args()
+    if args.command is None:
+        help_message = parser.format_help()
+        print(help_message)
+    else:
+        args.func()
+
+
+def info() -> None:
+    """Output diagnostic information."""
+    print(
+        f"{ASCII_ART}\n"
+        f"     movement: {movement.__version__}\n"
+        f"     Python: {platform.python_version()}\n"
+        f"     NumPy: {np.__version__}\n"
+        f"     xarray: {xr.__version__}\n"
+        f"     pandas: {pd.__version__}\n"
+        f"     Platform: {platform.platform()}\n"
+    )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ tests/test_unit/test_cli_entrypoint.py
@@ -0,0 +1,31 @@
+from contextlib import nullcontext as does_not_raise
+from unittest.mock import patch
+
+import pytest
+
+from movement.cli_entrypoint import main
+
+
+@pytest.mark.parametrize(
+    "command, expected_exception",
+    [
+        (
+            ["movement", "info"],
+            does_not_raise("Platform: "),
+        ),  # Valid arg
+        (
+            ["movement", "invalid"],
+            pytest.raises(SystemExit),
+        ),  # Invalid arg
+        (["movement"], does_not_raise("usage: movement")),  # Empty arg
+    ],
+)
+def test_entrypoint_command(command, expected_exception):
+    with (
+        patch("sys.argv", command),
+        patch("builtins.print") as mock_print,
+        expected_exception as e,
+    ):
+        main()
+        printed_message = " ".join(map(str, mock_print.call_args.args))
+        assert e in printed_message
