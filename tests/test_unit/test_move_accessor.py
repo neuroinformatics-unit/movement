a4d52326074c804d714b2c86d443418a698e9d76
sfmig <33267254+sfmig@users.noreply.github.com>
2024-08-01 16:54:00 +0100

    Adapt tests to new conftest

--- tests/test_unit/test_move_accessor.py
+++ tests/test_unit/test_move_accessor.py
@@ -4,36 +4,50 @@ import pytest
 import xarray as xr
 
 
-class TestMovementDataset:
-    """Test suite for the MovementDataset class."""
+@pytest.mark.parametrize(
+    "valid_dataset", ("valid_poses_dataset", "valid_bboxes_dataset")
+)
+def test_compute_kinematics_with_valid_dataset(
+    valid_dataset, kinematic_property, request
+):
+    """Test that computing a kinematic property of a valid
+    poses or bounding boxes dataset via accessor methods returns
+    an instance of xr.DataArray.
+    """
+    valid_input_dataset = request.getfixturevalue(valid_dataset)
 
-    def test_compute_kinematics_with_valid_dataset(
-        self, valid_poses_dataset, kinematic_property
-    ):
-        """Test that computing a kinematic property of a valid
-        pose dataset via accessor methods returns an instance of
-        xr.DataArray.
-        """
-        result = getattr(
-            valid_poses_dataset.move, f"compute_{kinematic_property}"
-        )()
-        assert isinstance(result, xr.DataArray)
+    result = getattr(
+        valid_input_dataset.move, f"compute_{kinematic_property}"
+    )()
+    assert isinstance(result, xr.DataArray)
 
-    def test_compute_kinematics_with_invalid_dataset(
-        self, invalid_poses_dataset, kinematic_property
-    ):
-        """Test that computing a kinematic property of an invalid
-        poses dataset via accessor methods raises the appropriate error.
-        """
-        expected_exception = (
-            RuntimeError
-            if isinstance(invalid_poses_dataset, xr.Dataset)
-            else AttributeError
-        )
-        with pytest.raises(expected_exception):
-            getattr(
-                invalid_poses_dataset.move, f"compute_{kinematic_property}"
-            )()
+
+@pytest.mark.parametrize(
+    "invalid_dataset",
+    (
+        "not_a_dataset",
+        "empty_dataset",
+        "missing_var_poses_dataset",
+        "missing_var_bboxes_dataset",
+        "missing_dim_poses_dataset",
+        "missing_dim_bboxes_dataset",
+    ),
+)
+def test_compute_kinematics_with_invalid_dataset(
+    invalid_dataset, kinematic_property, request
+):
+    """Test that computing a kinematic property of an invalid
+    poses or bounding boxes dataset via accessor methods raises
+    the appropriate error.
+    """
+    invalid_dataset = request.getfixturevalue(invalid_dataset)
+    expected_exception = (
+        RuntimeError
+        if isinstance(invalid_dataset, xr.Dataset)
+        else AttributeError
+    )
+    with pytest.raises(expected_exception):
+        getattr(invalid_dataset.move, f"compute_{kinematic_property}")()
 
 
 @pytest.mark.parametrize(
