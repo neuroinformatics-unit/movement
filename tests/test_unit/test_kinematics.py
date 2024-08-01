d9e9317d3bc9b816eaef006facec5446737bbc5f
sfmig <33267254+sfmig@users.noreply.github.com>
2024-08-01 16:55:38 +0100

    Adapt kinematics tests

--- tests/test_unit/test_kinematics.py
+++ tests/test_unit/test_kinematics.py
@@ -53,7 +53,7 @@ class TestKinematics:
     kinematic_test_params = [
         ("valid_poses_dataset", does_not_raise()),
         ("valid_poses_dataset_with_nan", does_not_raise()),
-        ("missing_dim_dataset", pytest.raises(AttributeError)),
+        ("missing_dim_poses_dataset", pytest.raises(AttributeError)),
     ]
 
     @pytest.mark.parametrize("ds, expected_exception", kinematic_test_params)
