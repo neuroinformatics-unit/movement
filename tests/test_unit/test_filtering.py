1fbe050ee0b42c3b230025e3b7c056b3a60a8aa3
sfmig <33267254+sfmig@users.noreply.github.com>
2024-08-07 13:16:21 +0100

    Combine median and savgol filter tests

--- tests/test_unit/test_filtering.py
+++ tests/test_unit/test_filtering.py
@@ -39,11 +39,6 @@ expected_n_nans_in_position_per_indiv = {
 }
 
 
-def _apply_filter_to_position(dataset, filter_func, **kwargs):
-    """Apply a filter function to the position data of a dataset."""
-    return filter_func(dataset.position, **kwargs)
-
-
 @pytest.mark.parametrize(
     "valid_dataset_with_nan",
     list_valid_datasets_with_nans,
@@ -125,9 +120,8 @@ def test_filter_by_confidence_on_position(
     """
     # Filter position by confidence
     valid_input_dataset = request.getfixturevalue(valid_dataset_no_nans)
-    position_filtered = _apply_filter_to_position(
-        valid_input_dataset,
-        filter_by_confidence,
+    position_filtered = filter_by_confidence(
+        valid_input_dataset.position,
         confidence=valid_input_dataset.confidence,
         threshold=0.6,
     )
@@ -148,17 +142,25 @@ def test_filter_by_confidence_on_position(
     "valid_dataset",
     list_all_valid_datasets,
 )
-@pytest.mark.parametrize("window_size", [2, 4])
-def test_median_filter_on_position(valid_dataset, window_size, request):
+@pytest.mark.parametrize(
+    ("filter_func, filter_kwargs"),
+    [
+        (median_filter, {"window": 2}),
+        (median_filter, {"window": 4}),
+        (savgol_filter, {"window": 2, "polyorder": 1}),
+        (savgol_filter, {"window": 4, "polyorder": 2}),
+    ],
+)
+def test_filter_on_position(
+    filter_func, filter_kwargs, valid_dataset, request
+):
     """Test that applying the median filter to the position data returns
     a different xr.DataArray than the input position data.
     """
-    # Filter position using median filter
+    # Filter position
     valid_input_dataset = request.getfixturevalue(valid_dataset)
-    position_filtered = _apply_filter_to_position(
-        valid_input_dataset,
-        median_filter,
-        window=window_size,
+    position_filtered = filter_func(
+        valid_input_dataset.position, **filter_kwargs
     )
 
     del position_filtered.attrs["log"]
@@ -174,7 +176,16 @@ def test_median_filter_on_position(valid_dataset, window_size, request):
     ("valid_dataset, expected_n_nans_in_position_per_indiv"),
     [(k, v) for k, v in expected_n_nans_in_position_per_indiv.items()],
 )
-def test_median_filter_with_nans_on_position(
+@pytest.mark.parametrize(
+    ("filter_func, filter_kwargs"),
+    [
+        (median_filter, {"window": 3}),
+        (savgol_filter, {"window": 3, "polyorder": 2}),
+    ],
+)
+def test_filter_with_nans_on_position(
+    filter_func,
+    filter_kwargs,
     valid_dataset,
     expected_n_nans_in_position_per_indiv,
     helpers,
@@ -183,109 +194,43 @@ def test_median_filter_with_nans_on_position(
     """Test NaN behaviour of the median filter. The median filter
     should propagate NaNs within the windows of the filter.
     """
-    # Filter position using median filter
-    valid_input_dataset = request.getfixturevalue(valid_dataset)
-    position_filtered = _apply_filter_to_position(
+
+    def _assert_n_nans_in_position_per_individual(
         valid_input_dataset,
-        median_filter,
-        window=3,
+        position_filtered,
+        expected_n_nans_in_position_per_indiv,
+    ):
+        # compute n nans in position after filtering per individual
+        n_nans_after_filtering_per_indiv = {
+            i: helpers.count_nans(position_filtered.isel(individuals=i))
+            for i in range(valid_input_dataset.dims["individuals"])
+        }
+
+        # check number of nans per indiv is as expected
+        for i in range(valid_input_dataset.dims["individuals"]):
+            assert n_nans_after_filtering_per_indiv[i] == (
+                expected_n_nans_in_position_per_indiv[i]
+                * valid_input_dataset.dims["space"]
+                * valid_input_dataset.dims.get("keypoints", 1)
+            )
+
+    # Filter position
+    valid_input_dataset = request.getfixturevalue(valid_dataset)
+    position_filtered = filter_func(
+        valid_input_dataset.position, **filter_kwargs
     )
 
-    # count nans in input
+    # check number of nans per indiv is as expected
+    _assert_n_nans_in_position_per_individual(
+        valid_input_dataset,
+        position_filtered,
+        expected_n_nans_in_position_per_indiv,
+    )
+
+    # if input had nans,
+    # individual 1's position at exact timepoints 0, 1 and 5 is not nan
     n_nans_input = helpers.count_nans(valid_input_dataset.position)
-
-    # count nans after filtering per individual
-    n_nans_after_filtering_per_indiv = {
-        i: helpers.count_nans(position_filtered.isel(individuals=i))
-        for i in range(valid_input_dataset.dims["individuals"])
-    }
-
-    # check number of nans is as expected
-    for i in range(valid_input_dataset.dims["individuals"]):
-        assert n_nans_after_filtering_per_indiv[i] == (
-            expected_n_nans_in_position_per_indiv[i]
-            * valid_input_dataset.dims["space"]
-            * valid_input_dataset.dims.get("keypoints", 1)
-        )
-
     if n_nans_input != 0:
-        # individual 1's position at exact timepoints 0, 1 and 5 is not nan
-        assert not (
-            position_filtered.isel(individuals=0, time=[0, 1, 5])
-            .isnull()
-            .any()
-        )
-
-
-@pytest.mark.parametrize(
-    "valid_dataset",
-    list_all_valid_datasets,
-)
-@pytest.mark.parametrize("window_size, polyorder", [(2, 1), (4, 2)])
-def test_savgol_filter_on_position(
-    valid_dataset, window_size, polyorder, request
-):
-    """Test that applying the Savitzky-Golay filter to the position data
-    returns a different xr.DataArray than the input position data.
-    """
-    # Filter position using Savitzky-Golay filter
-    valid_input_dataset = request.getfixturevalue(valid_dataset)
-    position_filtered = _apply_filter_to_position(
-        valid_input_dataset,
-        savgol_filter,
-        window=window_size,
-        polyorder=polyorder,
-    )
-
-    del position_filtered.attrs["log"]
-
-    # filtered array is an xr.DataArray
-    assert isinstance(position_filtered, xr.DataArray)
-
-    # filtered data should not be equal to the original data
-    assert not (position_filtered.equals(valid_input_dataset.position))
-
-
-@pytest.mark.parametrize(
-    ("valid_dataset, expected_n_nans_in_position_per_indiv"),
-    [(k, v) for k, v in expected_n_nans_in_position_per_indiv.items()],
-)
-def test_savgol_filter_with_nans_on_position(
-    valid_dataset, expected_n_nans_in_position_per_indiv, helpers, request
-):
-    """Test NaN behaviour of the Savitzky-Golay filter. The Savitzky-Golay
-    filter should propagate NaNs within the windows of the filter.
-    """
-    # Filter position using Savitzky-Golay filter
-    valid_input_dataset = request.getfixturevalue(valid_dataset)
-    position_filtered = _apply_filter_to_position(
-        valid_input_dataset,
-        savgol_filter,
-        window=3,
-        polyorder=2,
-    )
-
-    # count nans in input
-    n_nans_input = helpers.count_nans(valid_input_dataset.position)
-
-    # count nans after filtering per individual
-    n_nans_after_filtering_per_indiv = {
-        i: helpers.count_nans(position_filtered.isel(individuals=i))
-        for i in range(valid_input_dataset.dims["individuals"])
-    }
-
-    # check number of nans is as expected
-    for i in range(valid_input_dataset.dims["individuals"]):
-        assert n_nans_after_filtering_per_indiv[i] == (
-            expected_n_nans_in_position_per_indiv[i]
-            * valid_input_dataset.dims["space"]
-            * valid_input_dataset.dims.get("keypoints", 1)
-        )
-
-    # if input data had nans, the filtered position of individual 1
-    # will be nan at every timepoint except for timepoints 0, 1 and 5
-    if n_nans_input != 0:
-        # individual 1's position at exact timepoints 0, 1 and 5 is not nan
         assert not (
             position_filtered.isel(individuals=0, time=[0, 1, 5])
             .isnull()
