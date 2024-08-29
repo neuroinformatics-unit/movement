4fbd16d58a858d7c5c415fcd2a5689baa8c9afb8
sfmig <33267254+sfmig@users.noreply.github.com>
2024-08-29 12:08:38 +0100

    Check if coverage changes

--- tests/test_integration/test_kinematics_vector_transform.py
+++ tests/test_integration/test_kinematics_vector_transform.py
@@ -1,33 +1,33 @@
-from contextlib import nullcontext as does_not_raise
+# from contextlib import nullcontext as does_not_raise
 
-import pytest
-import xarray as xr
+# import pytest
+# import xarray as xr
 
-from movement.utils import vector
+# from movement.utils import vector
 
 
-class TestKinematicsVectorTransform:
-    """Test the vector transformation functionality with
-    various kinematic properties.
-    """
+# class TestKinematicsVectorTransform:
+#     """Test the vector transformation functionality with
+#     various kinematic properties.
+#     """
 
-    @pytest.mark.parametrize(
-        "ds, expected_exception",
-        [
-            ("valid_poses_dataset", does_not_raise()),
-            ("valid_poses_dataset_with_nan", does_not_raise()),
-            ("missing_dim_poses_dataset", pytest.raises(RuntimeError)),
-        ],
-    )
-    def test_cart_and_pol_transform(
-        self, ds, expected_exception, kinematic_property, request
-    ):
-        """Test transformation between Cartesian and polar coordinates
-        with various kinematic properties.
-        """
-        ds = request.getfixturevalue(ds)
-        with expected_exception:
-            data = getattr(ds.move, f"compute_{kinematic_property}")()
-            pol_data = vector.cart2pol(data)
-            cart_data = vector.pol2cart(pol_data)
-            xr.testing.assert_allclose(cart_data, data)
+#     @pytest.mark.parametrize(
+#         "ds, expected_exception",
+#         [
+#             ("valid_poses_dataset", does_not_raise()),
+#             ("valid_poses_dataset_with_nan", does_not_raise()),
+#             ("missing_dim_poses_dataset", pytest.raises(RuntimeError)),
+#         ],
+#     )
+#     def test_cart_and_pol_transform(
+#         self, ds, expected_exception, kinematic_property, request
+#     ):
+#         """Test transformation between Cartesian and polar coordinates
+#         with various kinematic properties.
+#         """
+#         ds = request.getfixturevalue(ds)
+#         with expected_exception:
+#             data = getattr(ds.move, f"compute_{kinematic_property}")()
+#             pol_data = vector.cart2pol(data)
+#             cart_data = vector.pol2cart(pol_data)
+#             xr.testing.assert_allclose(cart_data, data)
