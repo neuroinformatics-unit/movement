de4b839d368486d530d5a1dcbd512ce9d7e6bbd4
sfmig <33267254+sfmig@users.noreply.github.com>
2024-08-30 20:02:44 +0100

    Update integration tests for filtering

--- tests/test_integration/test_filtering.py
+++ tests/test_integration/test_filtering.py
@@ -16,63 +16,50 @@ def sample_dataset():
         "poses"
     ]
     ds = load_poses.from_dlc_file(ds_path)
-    ds["velocity"] = ds.move.compute_velocity()
+
     return ds
 
 
 @pytest.mark.parametrize("window", [3, 5, 6, 13])
 def test_nan_propagation_through_filters(sample_dataset, window, helpers):
-    """Test NaN propagation when passing a DataArray through
-    multiple filters sequentially. For the ``median_filter``
-    and ``savgol_filter``, the number of NaNs is expected to increase
+    """Test NaN propagation is as expected when passing a DataArray through
+    filter by confidence, Savgol filter and interpolation.
+    For the ``savgol_filter``, the number of NaNs is expected to increase
     at most by the filter's window length minus one (``window - 1``)
     multiplied by the number of consecutive NaNs in the input data.
     """
-    # Introduce nans via filter_by_confidence
+    # Check filter position by confidence behaves as expected
+    # default threshold is 0.6 for confidence
+    expected_n_nans = (sample_dataset.confidence.data < 0.6).sum()  # 13136
     sample_dataset.update(
         {"position": sample_dataset.move.filter_by_confidence()}
     )
-    expected_n_nans = 13136
-    n_nans_confilt = helpers.count_nans(sample_dataset.position)
-    assert n_nans_confilt == expected_n_nans, (
-        f"Expected {expected_n_nans} NaNs in filtered data, "
-        f"got: {n_nans_confilt}"
-    )
-    n_consecutive_nans = helpers.count_consecutive_nans(
-        sample_dataset.position
-    )
-    # Apply median filter and check that
-    # it doesn't introduce too many or too few NaNs
-    sample_dataset.update(
-        {"position": sample_dataset.move.median_filter(window)}
-    )
-    n_nans_medfilt = helpers.count_nans(sample_dataset.position)
-    max_nans_increase = (window - 1) * n_consecutive_nans
-    assert (
-        n_nans_medfilt <= n_nans_confilt + max_nans_increase
-    ), "Median filter introduced more NaNs than expected."
-    assert (
-        n_nans_medfilt >= n_nans_confilt
-    ), "Median filter mysteriously removed NaNs."
-    n_consecutive_nans = helpers.count_consecutive_nans(
-        sample_dataset.position
+    n_total_nans_input = helpers.count_nans(sample_dataset.position)
+
+    assert n_total_nans_input == expected_n_nans, (
+        f"Expected {expected_n_nans} NaNs in input data, "
+        f"got: {n_total_nans_input}"
     )
 
-    # Apply savgol filter and check that
-    # it doesn't introduce too many or too few NaNs
+    # Compute maximum expected increase in NaNs due to filtering
+    n_consecutive_nans_input = helpers.count_consecutive_nans(
+        sample_dataset.position
+    )
+    max_nans_increase = (window - 1) * n_consecutive_nans_input
+
+    # Apply savgol filter and check that number of NaNs is within threshold
     sample_dataset.update(
         {"position": sample_dataset.move.savgol_filter(window, polyorder=2)}
     )
-    n_nans_savgol = helpers.count_nans(sample_dataset.position)
-    max_nans_increase = (window - 1) * n_consecutive_nans
-    assert (
-        n_nans_savgol <= n_nans_medfilt + max_nans_increase
-    ), "Savgol filter introduced more NaNs than expected."
-    assert (
-        n_nans_savgol >= n_nans_medfilt
-    ), "Savgol filter mysteriously removed NaNs."
 
-    # Interpolate data (without max_gap) to eliminate all NaNs
+    n_total_nans_savgol = helpers.count_nans(sample_dataset.position)
+
+    # Check that filtering does not reduce number of nans
+    assert n_total_nans_savgol >= n_total_nans_input
+    # Check that the increase in nans is below the expected threshold
+    assert n_total_nans_savgol - n_total_nans_input <= max_nans_increase
+
+    # Interpolate data (without max_gap) and check it eliminates all NaNs
     sample_dataset.update(
         {"position": sample_dataset.move.interpolate_over_time()}
     )
