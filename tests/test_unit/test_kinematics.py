95965f8c22a9206b84670290e06d52eeec1a40d3
sfmig <33267254+sfmig@users.noreply.github.com>
2024-09-06 09:31:28 +0100

    Simplify and expand kinematic tests for bboxes (#265)
    * Simplify and expand kinematic tests
    
    * Suggestion to rename internal method for clarity
    
    * Update docstrings for API reference docs
    
    * Add test for values
    
    * Refactor test for uniform linear motion
    
    * Add notes to dosctrings
    
    * Fix kinematics tests
    
    * Add fixture with uniform linear motion for poses
    
    * Add poses dataset to linear uniform motion test
    
    * Add test for dataset with nans
    
    * Edits to docstrings
    
    * Remove circular fixture
    
    * Small edits to fixture comments
    
    * Edits to comments in tests
    
    * Small edits
    
    * Clarify vector vs array in docstrings and make consistent where required
    
    * Add missing docstring in test and small edits
    
    * Remove TODOs
    
    * Fix offset in fixture for uniform linear motion poses
    
    * Apply suggestions from code review
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>
    
    * Differentiation method
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>
    
    * Docstrings fixes
    
    * :py:meth: to :meth:
    
    * Combine into one paragraph
    
    * Add uniform linear motion to doscstring of fixtures
    
    * Simplify valid_poses_array_uniform_linear_motion with suggestions
    
    * kinematic_variable --> kinematic_array
    
    * Simplify test_kinematics_uniform_linear_motion with suggestion from review
    
    * Update tests/test_unit/test_kinematics.py
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>
    
    * Update tests/test_unit/test_kinematics.py
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>
    
    * Cosmetic edits to test
    
    * Change docstring to time-derivative
    
    ---------
    
    Co-authored-by: Chang Huan Lo <changhuan.lo@ucl.ac.uk>

--- tests/test_unit/test_kinematics.py
+++ tests/test_unit/test_kinematics.py
@@ -1,5 +1,3 @@
-from contextlib import nullcontext as does_not_raise
-
 import numpy as np
 import pytest
 import xarray as xr
@@ -7,106 +5,180 @@ import xarray as xr
 from movement.analysis import kinematics
 
 
-class TestKinematics:
-    """Test suite for the kinematics module."""
+@pytest.mark.parametrize(
+    "valid_dataset_uniform_linear_motion",
+    [
+        "valid_poses_dataset_uniform_linear_motion",
+        "valid_bboxes_dataset",
+    ],
+)
+@pytest.mark.parametrize(
+    "kinematic_variable, expected_kinematics",
+    [
+        (
+            "displacement",
+            [
+                np.vstack([np.zeros((1, 2)), np.ones((9, 2))]),  # Individual 0
+                np.multiply(
+                    np.vstack([np.zeros((1, 2)), np.ones((9, 2))]),
+                    np.array([1, -1]),
+                ),  # Individual 1
+            ],
+        ),
+        (
+            "velocity",
+            [
+                np.ones((10, 2)),  # Individual 0
+                np.multiply(
+                    np.ones((10, 2)), np.array([1, -1])
+                ),  # Individual 1
+            ],
+        ),
+        (
+            "acceleration",
+            [
+                np.zeros((10, 2)),  # Individual 0
+                np.zeros((10, 2)),  # Individual 1
+            ],
+        ),
+    ],
+)
+def test_kinematics_uniform_linear_motion(
+    valid_dataset_uniform_linear_motion,
+    kinematic_variable,
+    expected_kinematics,  # 2D: n_frames, n_space_dims
+    request,
+):
+    """Test computed kinematics for a uniform linear motion case.
 
-    @pytest.fixture
-    def expected_dataarray(self, valid_poses_dataset):
-        """Return a function to generate the expected dataarray
-        for different kinematic properties.
-        """
+    Uniform linear motion means the individuals move along a line
+    at constant velocity.
 
-        def _expected_dataarray(property):
-            """Return an xarray.DataArray with default values and
-            the expected dimensions and coordinates.
-            """
-            # Expected x,y values for velocity
-            x_vals = np.array(
-                [1.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 17.0]
-            )
-            y_vals = np.full((10, 2, 2, 1), 4.0)
-            if property == "acceleration":
-                x_vals = np.array(
-                    [1.0, 1.5, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.5, 1.0]
-                )
-                y_vals = np.full((10, 2, 2, 1), 0)
-            elif property == "displacement":
-                x_vals = np.array(
-                    [0.0, 1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0]
-                )
-                y_vals[0] = 0
+    We consider 2 individuals ("id_0" and "id_1"),
+    tracked for 10 frames, along x and y:
+    - id_0 moves along x=y line from the origin
+    - id_1 moves along x=-y line from the origin
+    - they both move one unit (pixel) along each axis in each frame
 
-            x_vals = x_vals.reshape(-1, 1, 1, 1)
-            # Repeat the x_vals to match the shape of the position
-            x_vals = np.tile(x_vals, (1, 2, 2, 1))
-            return xr.DataArray(
-                np.concatenate(
-                    [x_vals, y_vals],
-                    axis=-1,
-                ),
-                dims=valid_poses_dataset.dims,
-                coords=valid_poses_dataset.coords,
-            )
+    If the dataset is a poses dataset, we consider 3 keypoints per individual
+    (centroid, left, right), that are always in front of the centroid keypoint
+    at 45deg from the trajectory.
+    """
+    # Compute kinematic array from input dataset
+    position = request.getfixturevalue(
+        valid_dataset_uniform_linear_motion
+    ).position
+    kinematic_array = getattr(kinematics, f"compute_{kinematic_variable}")(
+        position
+    )
 
-        return _expected_dataarray
+    # Build expected data array from the expected numpy array
+    expected_array = xr.DataArray(
+        np.stack(expected_kinematics, axis=1),
+        # Stack along the "individuals" axis
+        dims=["time", "individuals", "space"],
+    )
+    if "keypoints" in position.coords:
+        expected_array = expected_array.expand_dims(
+            {"keypoints": position.coords["keypoints"].size}
+        )
+        expected_array = expected_array.transpose(
+            "time", "individuals", "keypoints", "space"
+        )
 
-    kinematic_test_params = [
-        ("valid_poses_dataset", does_not_raise()),
-        ("valid_poses_dataset_with_nan", does_not_raise()),
-        ("missing_dim_poses_dataset", pytest.raises(ValueError)),
+    # Compare the values of the kinematic_array against the expected_array
+    np.testing.assert_allclose(kinematic_array.values, expected_array.values)
+
+
+@pytest.mark.parametrize(
+    "valid_dataset_with_nan",
+    [
+        "valid_poses_dataset_with_nan",
+        "valid_bboxes_dataset_with_nan",
+    ],
+)
+@pytest.mark.parametrize(
+    "kinematic_variable, expected_nans_per_individual",
+    [
+        ("displacement", [5, 0]),  # individual 0, individual 1
+        ("velocity", [6, 0]),
+        ("acceleration", [7, 0]),
+    ],
+)
+def test_kinematics_with_dataset_with_nans(
+    valid_dataset_with_nan,
+    kinematic_variable,
+    expected_nans_per_individual,
+    helpers,
+    request,
+):
+    """Test kinematics computation for a dataset with nans.
+
+    We test that the kinematics can be computed and that the number
+    of nan values in the kinematic array is as expected.
+
+    """
+    # compute kinematic array
+    valid_dataset = request.getfixturevalue(valid_dataset_with_nan)
+    position = valid_dataset.position
+    kinematic_array = getattr(kinematics, f"compute_{kinematic_variable}")(
+        position
+    )
+
+    # compute n nans in kinematic array per individual
+    n_nans_kinematics_per_indiv = [
+        helpers.count_nans(kinematic_array.isel(individuals=i))
+        for i in range(valid_dataset.dims["individuals"])
     ]
 
-    @pytest.mark.parametrize("ds, expected_exception", kinematic_test_params)
-    def test_displacement(
-        self, ds, expected_exception, expected_dataarray, request
-    ):
-        """Test displacement computation."""
-        ds = request.getfixturevalue(ds)
-        with expected_exception:
-            result = kinematics.compute_displacement(ds.position)
-            expected = expected_dataarray("displacement")
-            if ds.position.isnull().any():
-                expected.loc[
-                    {"individuals": "ind1", "time": [3, 4, 7, 8, 9]}
-                ] = np.nan
-            xr.testing.assert_allclose(result, expected)
+    # expected nans per individual adjusted for space and keypoints dimensions
+    expected_nans_adjusted = [
+        n
+        * valid_dataset.dims["space"]
+        * valid_dataset.dims.get("keypoints", 1)
+        for n in expected_nans_per_individual
+    ]
+    # check number of nans per individual is as expected in kinematic array
+    np.testing.assert_array_equal(
+        n_nans_kinematics_per_indiv, expected_nans_adjusted
+    )
 
-    @pytest.mark.parametrize("ds, expected_exception", kinematic_test_params)
-    def test_velocity(
-        self, ds, expected_exception, expected_dataarray, request
-    ):
-        """Test velocity computation."""
-        ds = request.getfixturevalue(ds)
-        with expected_exception:
-            result = kinematics.compute_velocity(ds.position)
-            expected = expected_dataarray("velocity")
-            if ds.position.isnull().any():
-                expected.loc[
-                    {"individuals": "ind1", "time": [2, 4, 6, 7, 8, 9]}
-                ] = np.nan
-            xr.testing.assert_allclose(result, expected)
 
-    @pytest.mark.parametrize("ds, expected_exception", kinematic_test_params)
-    def test_acceleration(
-        self, ds, expected_exception, expected_dataarray, request
-    ):
-        """Test acceleration computation."""
-        ds = request.getfixturevalue(ds)
-        with expected_exception:
-            result = kinematics.compute_acceleration(ds.position)
-            expected = expected_dataarray("acceleration")
-            if ds.position.isnull().any():
-                expected.loc[
-                    {"individuals": "ind1", "time": [1, 3, 5, 6, 7, 8, 9]}
-                ] = np.nan
-            xr.testing.assert_allclose(result, expected)
+@pytest.mark.parametrize(
+    "invalid_dataset, expected_exception",
+    [
+        ("not_a_dataset", pytest.raises(AttributeError)),
+        ("empty_dataset", pytest.raises(AttributeError)),
+        ("missing_var_poses_dataset", pytest.raises(AttributeError)),
+        ("missing_var_bboxes_dataset", pytest.raises(AttributeError)),
+        ("missing_dim_poses_dataset", pytest.raises(ValueError)),
+        ("missing_dim_bboxes_dataset", pytest.raises(ValueError)),
+    ],
+)
+@pytest.mark.parametrize(
+    "kinematic_variable",
+    [
+        "displacement",
+        "velocity",
+        "acceleration",
+    ],
+)
+def test_kinematics_with_invalid_dataset(
+    invalid_dataset,
+    expected_exception,
+    kinematic_variable,
+    request,
+):
+    """Test kinematics computation with an invalid dataset."""
+    with expected_exception:
+        position = request.getfixturevalue(invalid_dataset).position
+        getattr(kinematics, f"compute_{kinematic_variable}")(position)
 
-    @pytest.mark.parametrize("order", [0, -1, 1.0, "1"])
-    def test_approximate_derivative_with_invalid_order(self, order):
-        """Test that an error is raised when the order is non-positive."""
-        data = np.arange(10)
-        expected_exception = (
-            ValueError if isinstance(order, int) else TypeError
-        )
-        with pytest.raises(expected_exception):
-            kinematics._compute_approximate_time_derivative(data, order=order)
+
+@pytest.mark.parametrize("order", [0, -1, 1.0, "1"])
+def test_approximate_derivative_with_invalid_order(order):
+    """Test that an error is raised when the order is non-positive."""
+    data = np.arange(10)
+    expected_exception = ValueError if isinstance(order, int) else TypeError
+    with pytest.raises(expected_exception):
+        kinematics._compute_approximate_time_derivative(data, order=order)
