4b40a22ccd6d1f7b82eb5ee03f0c437978763b1c
niksirbi <niko.sirbiladze@gmail.com>
2024-12-18 18:48:45 +0000

    simplified private nwb functions

--- movement/io/load_poses.py
+++ movement/io/load_poses.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from typing import Literal
 
 import h5py
-import ndx_pose
 import numpy as np
 import pandas as pd
 import pynwb
@@ -917,66 +916,29 @@ def _ds_from_nwb_object(
     pose_estimation = nwb_file.processing["behavior"][key_name]
     source_software = pose_estimation.fields["source_software"]
     pose_estimation_series = pose_estimation.fields["pose_estimation_series"]
-    single_keypoint_datasets = [
-        _ds_from_pose_estimation_series(
-            pes,
-            keypoint,
-            subject_name=nwb_file.identifier,
-            source_software=source_software,
+    single_keypoint_datasets = []
+    for keypoint, pse in pose_estimation_series.items():
+        # Extract position and confidence data for each keypoint
+        position_data = np.asarray(pse.data)  # shape: (n_time, n_space)
+        confidence_data = (  # shape: (n_time,)
+            np.asarray(pse.confidence)
+            if getattr(pse, "confidence", None) is not None
+            else np.full(position_data.shape[0], np.nan)
         )
-        for keypoint, pes in pose_estimation_series.items()
-    ]
+
+        # Compute fps from the time differences between timestamps
+        fps = np.nanmedian(1 / np.diff(pse.timestamps))
+
+        single_keypoint_datasets.append(
+            # create movement dataset with 1 keypoint and 1 individual
+            from_numpy(
+                position_data[:, :, np.newaxis, np.newaxis],
+                confidence_data[:, np.newaxis, np.newaxis],
+                individual_names=[nwb_file.identifier],
+                keypoint_names=[keypoint],
+                fps=round(fps, 6),
+                source_software=source_software,
+            )
+        )
 
     return xr.merge(single_keypoint_datasets)
-
-
-def _ds_from_pose_estimation_series(
-    pose_estimation_series: ndx_pose.PoseEstimationSeries,
-    keypoint: str,
-    subject_name: str,
-    source_software: str,
-) -> xr.Dataset:
-    """Create a ``movement`` poses dataset from a pose estimation series.
-
-    Note that the ``PoseEstimationSeries`` object only contains data for a
-    single point over time.
-
-    Parameters
-    ----------
-    pose_estimation_series : ndx_pose.PoseEstimationSeries
-        PoseEstimationSeries NWB object to be converted
-    keypoint : str
-        Name of the keypoint (body part)
-    subject_name : str
-        Name of the subject (individual)
-    source_software : str
-        Name of the software used to estimate the pose
-
-    Returns
-    -------
-    movement_dataset : xr.Dataset
-        A single-individual ``movement`` poses dataset with one keypoint
-
-    """
-    # extract pose_estimation series data (n_time, n_space)
-    position_array = np.asarray(pose_estimation_series.data)
-
-    # extract confidence data (n_time,)
-    if getattr(pose_estimation_series, "confidence", None) is not None:
-        confidence_array = np.asarray(pose_estimation_series.confidence)
-    else:
-        confidence_array = np.full(position_array.shape[0], np.nan)
-
-    # Compute fps from the time differences between timestamps
-    fps = np.nanmedian(1 / np.diff(pose_estimation_series.timestamps))
-
-    # create movement dataset with 1 keypoint and 1 individual
-    ds = from_numpy(
-        position_array[:, :, np.newaxis, np.newaxis],  # n_time, n_space, 1, 1
-        confidence_array[:, np.newaxis, np.newaxis],  # n_time, 1, 1
-        individual_names=[subject_name],
-        keypoint_names=[keypoint],
-        fps=round(fps, 6),
-        source_software=source_software,
-    )
-    return ds
