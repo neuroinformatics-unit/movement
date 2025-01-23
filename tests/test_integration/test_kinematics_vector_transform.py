d4013fce1e1bd8067dcfa6f690848d3adde9a8a8
lochhh <changhuan.lo@ucl.ac.uk>
2025-01-23 19:19:06 +0000

    Shorten arg name

--- tests/test_integration/test_kinematics_vector_transform.py
+++ tests/test_integration/test_kinematics_vector_transform.py
@@ -9,8 +9,7 @@ from movement.utils import vector
 
 
 @pytest.mark.parametrize(
-    "valid_dataset_uniform_linear_motion",
-    ["valid_poses_dataset", "valid_bboxes_dataset"],
+    "valid_dataset", ["valid_poses_dataset", "valid_bboxes_dataset"]
 )
 @pytest.mark.parametrize(
     "kinematic_variable, expected_kinematics_polar",
@@ -53,15 +52,12 @@ from movement.utils import vector
     ],
 )
 def test_cart2pol_transform_on_kinematics(
-    valid_dataset_uniform_linear_motion,
-    kinematic_variable,
-    expected_kinematics_polar,
-    request,
+    valid_dataset, kinematic_variable, expected_kinematics_polar, request
 ):
     """Test transformation between Cartesian and polar coordinates
     with various kinematic properties.
     """
-    ds = request.getfixturevalue(valid_dataset_uniform_linear_motion)
+    ds = request.getfixturevalue(valid_dataset)
     kinematic_array_cart = getattr(kin, f"compute_{kinematic_variable}")(
         ds.position
     )
