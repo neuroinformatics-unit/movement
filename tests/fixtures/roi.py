57edda6a7bd7b5a23ef13504e580d630b8b3deac
willGraham01 <1willgraham@gmail.com>
2025-02-21 10:09:41 +0000

    Move conftest to fixtures directory

--- tests/fixtures/roi.py
+++ tests/fixtures/roi.py
@@ -14,6 +14,10 @@ def segment_of_y_equals_x() -> LineOfInterest:
 
 @pytest.fixture()
 def unit_square_pts() -> np.ndarray:
+    """Points that define the 4 corners of a unit-length square.
+
+    The points have the lower-left corner positioned at (0,0).
+    """
     return np.array(
         [
             [0.0, 0.0],
@@ -33,6 +37,7 @@ def unit_square_hole(unit_square_pts: np.ndarray) -> np.ndarray:
 
 @pytest.fixture
 def unit_square(unit_square_pts: xr.DataArray) -> PolygonOfInterest:
+    """Square of unit side-length centred on (0.5, 0.5)."""
     return PolygonOfInterest(unit_square_pts, name="Unit square")
 
 
@@ -40,6 +45,12 @@ def unit_square(unit_square_pts: xr.DataArray) -> PolygonOfInterest:
 def unit_square_with_hole(
     unit_square_pts: xr.DataArray, unit_square_hole: xr.DataArray
 ) -> PolygonOfInterest:
+    """Square of unit side length with an internal hole.
+
+    The "outer" square is centred on (0.5, 0.5) and has side length 1.
+    The "inner" square, or hole, is centred on (0.5, 0.5) and has side length
+    0.5.
+    """
     return PolygonOfInterest(
         unit_square_pts, holes=[unit_square_hole], name="Unit square with hole"
     )
