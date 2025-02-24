541eb1d4098e3513ee109f05cd5ae8c4f3cef908
willGraham01 <1willgraham@gmail.com>
2025-02-24 13:29:19 +0000

    Read data and plot in image, need to fix the region definition

--- examples/boundary_angles.py
+++ examples/boundary_angles.py
@@ -13,10 +13,12 @@ using ``movement``'s Regions of Interest.
 # the following lines in your notebook
 # %matplotlib widget
 import matplotlib.pyplot as plt
+import numpy as np
 import xarray as xr
 
 from movement import sample_data
 from movement.plots import plot_trajectory
+from movement.roi import PolygonOfInterest
 
 # %%
 # Load Sample Dataset
@@ -27,17 +29,71 @@ from movement.plots import plot_trajectory
 
 ds = sample_data.fetch_dataset("SLEAP_three-mice_Aeon_proofread.analysis.h5")
 positions: xr.DataArray = ds.position
+
 # %%
 # The individuals in this dataset follow very similar, arc-like trajectories.
 # This is because they are actually confined to an arena in the shape of an
-# "octagonal doughnut".
-# We can define the arena as a region of interest to aid with our analysis of
-# the animals' behaviours. In this example, we are interested in the egocentric
-# and allocentric angles between the individuals' forward direction (their
-# direction of travel) and their approach to the closest part of the enclosure
-# wall.
+# "octadecagonal doughnut".
 
+# %%
+# Define Regions of Interest
+# --------------------------
+# First, we should define our arena as a region of interest. Since our arena is
+# two-dimensional, we use a ``PolygonOfInterest`` to describe it
+# programmatically.
+# The future napari plugin will support creating regions of interest by
+# clicking points and drawing shapes in the napari GUI. For the time being, we
+# can still define our arena by specifying the points that make up the interior
+# and exterior boundaries.
+centre = np.array([712.5, 541])
+width = 22
+extent = 1078  # 1080 - 2 or 1247-178, there's some give-and-take
+
+# Create the vertices of a "unit" octadecagon, centred on (0,0)
+n_pts = 18
+unit_shape = np.array(
+    [
+        np.exp((np.pi / 2.0 + (2.0 * i * np.pi) / n_pts) * 1.0j)
+        for i in range(n_pts)
+    ],
+    dtype=complex,
+)
+# Then stretch and translate the reference to match our arena
+outer_boundary = extent / 2.0 * unit_shape.copy()
+outer_boundary = (
+    np.array([outer_boundary.real, outer_boundary.imag]).transpose() + centre
+)
+inner_boundary = (extent - width) / 2.0 * unit_shape.copy()
+inner_boundary = (
+    np.array([inner_boundary.real, inner_boundary.imag]).transpose() + centre
+)
+
+# Create our Region of Interest from the outer boundary points, and include a
+# hole defined by the inner boundary points.
+arena = PolygonOfInterest(outer_boundary, holes=[inner_boundary], name="Arena")
+
+# %%
+# We can plot what we have created to provide a visual aid.
 fig, ax = plt.subplots(1, 1)
+
+# Plot the RegionOfInterest
+ax.plot(
+    [c[0] for c in arena.exterior_boundary.coords],
+    [c[1] for c in arena.exterior_boundary.coords],
+    "black",
+    lw=0.5,
+)
+ax.plot(
+    [c[0] for c in arena.interior_boundaries[0].coords],
+    [c[1] for c in arena.interior_boundaries[0].coords],
+    "black",
+    lw=0.5,
+)
+img = plt.imread(
+    "/home/ccaegra/.movement/data/frames/three-mice_Aeon_frame-5sec.png"
+)
+ax.imshow(img)
+
 for mouse_name, col in zip(
     positions.individuals.values, ["r", "g", "b"], strict=False
 ):
@@ -59,13 +115,8 @@ ax.set_xlabel("x (pixels)")
 ax.set_ylabel("y (pixels)")
 ax.legend()
 
+arena = PolygonOfInterest(outer_boundary, holes=[inner_boundary], name="Arena")
+
+d = arena.contains_point(positions)
+
 # %%
-# Define Regions of Interest
-# --------------------------
-# First, we should define our arena as a region of interest. Since our arena is
-# two-dimensional, we use a ``PolygonOfInterest`` to describe it
-# programmatically.
-# The future napari plugin will support creating regions of interest by
-# clicking points and drawing shapes in the napari GUI. For the time being, we
-# can still define our arena by specifying the points that make up the interior
-# and exterior boundaries.
