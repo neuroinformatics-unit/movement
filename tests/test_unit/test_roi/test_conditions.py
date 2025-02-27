c667e67f81442378bd1470195190649744c7b004
Stella <30465823+stellaprins@users.noreply.github.com>
2025-02-27 12:01:32 +0000

    simplify tests, test correct suffixing of occupancies when many regions with similar names are passed

--- tests/test_unit/test_roi/test_conditions.py
+++ tests/test_unit/test_roi/test_conditions.py
@@ -61,73 +61,37 @@ def triangle_moved_100(triangle_coords) -> PolygonOfInterest:
             ["triangle", "triangle", "triangle"],
             np.array([[0.15, 0.15], [0.85, 0.85], [0.5, 0.5], [1.5, 1.5]]),
             {
-                "data": np.array(
-                    [
-                        [True, False, True, False],
-                        [True, False, True, False],
-                        [True, False, True, False],
-                    ]
-                ),
+                "data": np.array([[True, False, True, False]] * 3),
                 "coords": ["triangle_0", "triangle_1", "triangle_2"],
             },
             id="3 superimposed triangles with same name",
         ),
         pytest.param(
             ["triangle", "triangle_different_name"],
-            np.array([[0.15, 0.15], [0.85, 0.85], [0.5, 0.5], [1.5, 1.5]]),
+            np.array([[0.5, 0.5]]),
             {
-                "data": np.array(
-                    [
-                        [True, False, True, False],
-                        [True, False, True, False],
-                    ]
-                ),
+                "data": np.array([True] * 2),
                 "coords": ["triangle", "pizza_slice"],
             },
             id="2 superimposed triangles with different names",
         ),
         pytest.param(
-            ["triangle", "triangle_moved_01"],
-            np.array([[0.15, 0.15], [0.85, 0.85], [0.5, 0.5], [1.5, 1.5]]),
-            {
-                "data": np.array(
-                    [
-                        [True, False, True, False],
-                        [True, False, True, False],
-                    ]
-                ),
-                "coords": ["triangle_0", "triangle_1"],
-            },
-            id="2 different triangles with same name",
-        ),
-        pytest.param(
             ["triangle", "triangle_moved_01", "triangle_moved_100"],
-            np.array([[0.15, 0.15], [0.85, 0.85], [0.5, 0.5], [1.5, 1.5]]),
+            np.array([[0.5, 0.5]]),
             {
-                "data": np.array(
-                    [
-                        [True, False, True, False],
-                        [True, False, True, False],
-                        [False, False, False, True],
-                    ]
-                ),
+                "data": np.array([[True], [True], [False]]),
                 "coords": ["triangle_0", "triangle_1", "triangle_2"],
             },
             id="3 different triangles with same name",
         ),
         pytest.param(
-            ["triangle", "unit_square"],
-            np.array([[0.15, 0.15], [0.5, 0.5]]),
+            ["triangle_different_name"],
+            np.array([[0.5, 0.5]]),
             {
-                "data": np.array(
-                    [
-                        [True, True],
-                        [True, True],
-                    ]
-                ),
-                "coords": ["triangle", "Unit square"],
+                "data": np.array([[True]]),
+                "coords": ["pizza_slice"],
             },
-            id="triangle, square, data points occupy both regions",
+            id="1 pizza slice triangle",
         ),
     ],
 )
@@ -137,7 +101,11 @@ def test_region_occupancy(
     data,
     expected_output: dict,
 ) -> None:
-    """Tests region_occupancy for several RoIs."""
+    """Tests region_occupancy for several RoIs.
+
+    Checks whether the dimension, data, and coordinates of the computed
+    occupancies are correct.
+    """
     regions = [request.getfixturevalue(r) for r in region_fixtures]
     data = xr.DataArray(
         data=data,
@@ -151,20 +119,39 @@ def test_region_occupancy(
     assert occupancies.occupancy.values.tolist() == expected_output["coords"]
 
 
-def test_region_occupancy_1000_triangles(triangle):
-    """Tests region_occupancy 1000 triangles with the same name."""
-    regions = [triangle] * 1000
+def test_region_occupancy_many_regions(
+    triangle, unit_square, unit_square_with_hole, triangle_different_name
+):
+    """Tests occupancy for many RoIs with similar names.
+
+    Ensures correct data and coordinate names for:
+        - 1000 triangles suffixed _000 to _999
+        - 100 unit squares suffixed _00 to _99
+        - 10 unit squares with holes suffixed _0 to _9
+        - 1 triangle named "pizza_slice" without suffix
+    """
+    regions = (
+        [triangle] * 1000
+        + [unit_square] * 100
+        + [unit_square_with_hole] * 10
+        + [triangle_different_name] * 1
+    )
+
     data = xr.DataArray(
-        data=np.array([[0.15, 0.15], [0.85, 0.85], [0.5, 0.5], [1.5, 1.5]]),
+        data=np.array([[0.5, 0.5]]),
         dims=["time", "space"],
         coords={"space": ["x", "y"]},
     )
     expected_output = {
-        "data": np.array([[True, False, True, False]] * 1000),
-        "coords": [f"triangle_{i:03d}" for i in range(1000)],
+        "data": np.array([[True]] * 1100 + [[False]] * 10 + [[True]] * 1),
+        "coords": (
+            [f"triangle_{i:03d}" for i in range(1000)]
+            + [f"Unit square_{i:02d}" for i in range(100)]
+            + [f"Unit square with hole_{i:01d}" for i in range(10)]
+            + ["pizza_slice"]
+        ),
     }
     occupancies = compute_region_occupancy(data, regions)
 
-    assert occupancies.dims == ("occupancy", "time")
-    assert (expected_output["data"] == occupancies.data).all()
+    assert (occupancies.data == expected_output["data"]).all()
     assert occupancies.occupancy.values.tolist() == expected_output["coords"]
