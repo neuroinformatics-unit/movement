30d7fa4a75b806dd24c14615fcd508a49b841bbc
willGraham01 <1willgraham@gmail.com>
2025-02-27 14:46:32 +0000

    Test that we can actually produce plots

--- tests/test_unit/test_roi/test_plot.py
+++ tests/test_unit/test_roi/test_plot.py
@@ -1,21 +1,61 @@
-from movement.roi import PolygonOfInterest
+from typing import Any
+
+import pytest
+from matplotlib.lines import Line2D
+from matplotlib.patches import PathPatch
+
+from movement.roi import LineOfInterest, PolygonOfInterest
+from movement.roi.base import BaseRegionOfInterest
 
 
-def test_plot() -> None:
-    cc_shell = [(0, 0), (1, 0), (1, 1), (0, 1)]
-    c_shell = [(0, 0), (0, 1), (1, 1), (1, 0)]
+@pytest.fixture
+def havarti() -> PolygonOfInterest:
+    """Triangular RoI with several holes.
 
-    cc_hole = [(0.25, 0.25), (0.75, 0.25), (0.75, 0.75), (0.25, 0.75)]
-    c_hole = [(0.25, 0.25), (0.25, 0.75), (0.75, 0.75), (0.75, 0.25)]
-
-    c_c = PolygonOfInterest(
-        c_shell,
-        holes=[c_hole],
+    Havarti is a type of cheese, FYI.
+    """
+    wedge = [(0, 0), (4, 0), (4, 0.5), (0, 2)]
+    hole1 = [(0.5, 0.5), (0.5, 0.75), (0.625, 0.625)]
+    hole2 = [(3, 0.25), (3.25, 0.5), (3.25, 0.75), (2.75, 0.75), (2.625, 0.5)]
+    hole3 = [(1.0, 0.1), (2, 0.1), (1.75, 0.2), (1.5, 0.3), (1.25, 0.2)]
+    hole4 = [(0.5, 1.75), (0.75, 1.5), (1.0, 1.25), (1.25, 1.25), (1.5, 1.4)]
+    return PolygonOfInterest(
+        wedge, holes=[hole1, hole2, hole3, hole4], name="Havarti"
     )
-    c_cc = PolygonOfInterest(c_shell, holes=[cc_hole])
-    cc_c = PolygonOfInterest(cc_shell, holes=[c_hole])
-    cc_cc = PolygonOfInterest(cc_shell, holes=[cc_hole])
 
-    for p in [c_c, c_cc, cc_c, cc_cc]:
-        fig, ax = p.plot()
-        pass
+
+@pytest.mark.parametrize(
+    ["region_to_plot", "kwargs"],
+    [
+        pytest.param("unit_square", {}, id="Unit square"),
+        pytest.param("unit_square_with_hole", {}, id="Unit square with hole"),
+        pytest.param(
+            "havarti",
+            {"facecolor": "yellow", "edgecolor": "black"},
+            id="Cheese",
+        ),
+        pytest.param(
+            LineOfInterest([(0.0, 0.0), (1.0, 0.0)]), {}, id="Segment"
+        ),
+        pytest.param(
+            LineOfInterest([(0.0, 0.0), (1.0, 0), (1.0, 1.0), (0.0, 1)]),
+            {},
+            id="Multi-segment",
+        ),
+    ],
+)
+def test_plot(
+    region_to_plot: BaseRegionOfInterest,
+    kwargs: dict[str, Any],
+    request,
+) -> None:
+    if isinstance(region_to_plot, str):
+        region_to_plot = request.getfixturevalue(region_to_plot)
+
+    _, ax = region_to_plot.plot(**kwargs)
+    if region_to_plot.dimensions == 2:
+        assert len(ax.patches) == 1 and len(ax.lines) == 0
+        assert type(ax.patches[0]) is PathPatch
+    else:
+        assert len(ax.patches) == 0 and len(ax.lines) == 1
+        assert type(ax.lines[0]) is Line2D
