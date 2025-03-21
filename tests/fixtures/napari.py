748dabb8ec4fc118100249e9f25919fc1332bbd8
SanjayUG <ganjekarsanjay6@gmail.com>
2025-03-21 21:51:20 +0530

    test: extend dimension slider tests for bounding box data

--- tests/fixtures/napari.py
+++ tests/fixtures/napari.py
@@ -1,7 +1,9 @@
+"""Fixtures for napari plugin tests."""
+
 import numpy as np
 import pytest
 
-from movement.io import save_poses
+from movement.io import save_poses, save_bboxes
 
 
 @pytest.fixture
@@ -50,3 +52,47 @@ def valid_dataset_with_localised_nans(valid_poses_dataset, tmp_path):
         return (out_path, valid_poses_dataset)
 
     return _valid_dataset_with_localised_nans
+
+
+@pytest.fixture
+def valid_bboxes_dataset_with_localised_nans(valid_bboxes_dataset, tmp_path):
+    """Return a factory of (path, dataset) pairs representing
+    valid bounding box datasets with NaN values at specific locations.
+    """
+
+    def _valid_bboxes_dataset_with_localised_nans(nan_location):
+        """Return a valid bounding box dataset and corresponding file with NaN values
+        at specific locations.
+
+        The ``nan_location`` parameter is a dictionary that specifies which
+        coordinates to set to NaN.
+
+        The dataset is modified from the `valid_bboxes_dataset` which represents
+        2 individuals ("id_0" and "id_1") moving in uniform linear motion for 10 frames
+        in 2D space.
+        """
+        # Express NaN location in time in "time" coordinates
+        if nan_location["time"] == "start":
+            time_point = 0
+        elif nan_location["time"] == "middle":
+            time_point = valid_bboxes_dataset.coords["time"][
+                valid_bboxes_dataset.coords["time"].shape[0] // 2
+            ]
+        elif nan_location["time"] == "end":
+            time_point = valid_bboxes_dataset.coords["time"][-1]
+
+        # Set the selected values to NaN
+        valid_bboxes_dataset.position.loc[
+            {
+                "individuals": nan_location["individuals"],
+                "time": time_point,
+            }
+        ] = np.nan
+
+        # Export as a VIA-tracks CSV file
+        out_path = tmp_path / "ds_with_nans.csv"
+        save_bboxes.to_via_tracks_file(valid_bboxes_dataset, out_path)
+
+        return (out_path, valid_bboxes_dataset)
+
+    return _valid_bboxes_dataset_with_localised_nans
