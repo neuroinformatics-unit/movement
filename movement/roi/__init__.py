270dd366ffd4c7605e35a9ed82ec1b14b2159b08
Harsh Bhanushali <109029538+harsh-bhanushali-05@users.noreply.github.com>
2025-04-06 00:03:38 +0530

    Linking class change

--- movement/roi/__init__.py
+++ movement/roi/__init__.py
@@ -2,4 +2,4 @@ from movement.roi.conditions import compute_region_occupancy
 from movement.roi.line import LineOfInterest
 from movement.roi.polygon import PolygonOfInterest
 
-__all__ = [LineOfInterest, PolygonOfInterest, compute_region_occupancy]
+__all__ = ['LineOfInterest', 'PolygonOfInterest', 'compute_region_occupancy']
