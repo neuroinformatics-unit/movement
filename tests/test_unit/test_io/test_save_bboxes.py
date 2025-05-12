a62e9a1eeeee722486b9b138ac2025c8289ef163
sfmig <33267254+sfmig@users.noreply.github.com>
2025-05-12 18:42:49 +0100

    Reorder tests

--- tests/test_unit/test_io/test_save_bboxes.py
+++ tests/test_unit/test_io/test_save_bboxes.py
@@ -21,225 +21,6 @@ def mock_csv_writer():
 
 
 @pytest.mark.parametrize(
-    "confidence",
-    [None, 0.5],
-    ids=["without_confidence", "with_confidence"],
-)
-@pytest.mark.parametrize(
-    "image_size",
-    [None, 100],
-    ids=["without_image_size", "with_image_size"],
-)
-@pytest.mark.parametrize(
-    "img_filename_template",
-    ["{:05d}.png", "{:03d}.jpg", "frame_{:03d}.jpg"],
-    ids=["png_extension", "jpg_extension", "frame_prefix"],
-)
-def test_write_single_row(
-    mock_csv_writer,
-    confidence,
-    image_size,
-    img_filename_template,
-):
-    """Test writing a single row of the VIA-tracks CSV file."""
-    # Fixed input values
-    frame, track_id, xy_coordinates, wh_values = (
-        1,
-        0,
-        np.array([100, 200]),
-        np.array([50, 30]),
-    )
-
-    # Write single row of VIA-tracks CSV file
-    with patch("csv.writer", return_value=mock_csv_writer):
-        row = _write_single_row(
-            mock_csv_writer,
-            xy_coordinates,
-            wh_values,
-            confidence,
-            track_id,
-            frame,
-            img_filename_template,
-            image_size,
-        )
-        mock_csv_writer.writerow.assert_called_with(row)
-
-    # Compute expected values
-    expected_filename = img_filename_template.format(frame)
-    expected_file_size = image_size if image_size is not None else 0
-    expected_file_attributes = "{}"  # placeholder value
-    expected_region_count = 0  # placeholder value
-    expected_region_id = 0  # placeholder value
-    expected_region_shape_attributes = {
-        "name": "rect",
-        "x": float(xy_coordinates[0] - wh_values[0] / 2),
-        "y": float(xy_coordinates[1] - wh_values[1] / 2),
-        "width": float(wh_values[0]),
-        "height": float(wh_values[1]),
-    }
-    expected_region_attributes = (
-        f'{{"track":"{int(track_id)}", "confidence":"{confidence}"}}'
-        if confidence is not None
-        else f'{{"track":"{int(track_id)}"}}'
-    )
-
-    # Check values are as expected
-    assert row[0] == expected_filename
-    assert row[1] == expected_file_size
-    assert row[2] == expected_file_attributes
-    assert row[3] == expected_region_count
-    assert row[4] == expected_region_id
-    assert row[5] == f"{expected_region_shape_attributes}"
-    assert row[6] == f"{expected_region_attributes}"
-
-
-@pytest.mark.parametrize(
-    "list_individuals, expected_track_id",
-    [
-        (["id_1", "id_3", "id_2"], [1, 3, 2]),
-        (["id_1", "id_2", "id_3"], [1, 2, 3]),
-        (["id-1", "id-2", "id-3"], [1, 2, 3]),
-        (["id1", "id2", "id3"], [1, 2, 3]),
-        (["id101", "id2", "id333"], [101, 2, 333]),
-        (["mouse_0_id1", "mouse_0_id2"], [1, 2]),
-    ],
-    ids=[
-        "unsorted",
-        "sorted",
-        "underscores",
-        "dashes",
-        "multiple_digits",
-        "middle_and_end_digits",
-    ],
-)
-def test_map_individuals_to_track_ids_from_individuals_names(
-    list_individuals, expected_track_id
-):
-    """Test the mapping individuals to track IDs if the track ID is
-    extracted from the individuals' names.
-    """
-    # Map individuals to track IDs
-    map_individual_to_track_id = _get_map_individuals_to_track_ids(
-        list_individuals, extract_track_id_from_individuals=True
-    )
-
-    # Check values are as expected
-    assert [
-        map_individual_to_track_id[individual]
-        for individual in list_individuals
-    ] == expected_track_id
-
-
-@pytest.mark.parametrize(
-    "list_individuals, expected_track_id",
-    [
-        (["A", "B", "C"], [0, 1, 2]),
-        (["C", "B", "A"], [2, 1, 0]),
-        (["id99", "id88", "id77"], [2, 1, 0]),
-    ],
-    ids=["sorted", "unsorted", "ignoring_digits"],
-)
-def test_map_individuals_to_track_ids_factorised(
-    list_individuals, expected_track_id
-):
-    """Test the mapping individuals to track IDs if the track ID is
-    factorised from the sorted individuals' names.
-    """
-    # Map individuals to track IDs
-    map_individual_to_track_id = _get_map_individuals_to_track_ids(
-        list_individuals, extract_track_id_from_individuals=False
-    )
-
-    # Check values are as expected
-    assert [
-        map_individual_to_track_id[individual]
-        for individual in list_individuals
-    ] == expected_track_id
-
-
-@pytest.mark.parametrize(
-    "list_individuals, expected_error_message",
-    [
-        (
-            ["mouse_1_id0", "mouse_2_id0"],
-            (
-                "Could not extract a unique track ID for all individuals. "
-                "Expected 2 unique track IDs, but got 1."
-            ),
-        ),
-        (
-            ["mouse_id1.0", "mouse_id2.0"],
-            (
-                "Could not extract a unique track ID for all individuals. "
-                "Expected 2 unique track IDs, but got 1."
-            ),
-        ),
-        (["A", "B", "C", "D"], "Could not extract track ID from A."),
-    ],
-    ids=["id_clash_1", "id_clash_2", "individuals_without_digits"],
-)
-def test_map_individuals_to_track_ids_error(
-    list_individuals, expected_error_message
-):
-    """Test that an error is raised if extracting track IDs from the
-    individuals' names fails.
-    """
-    with pytest.raises(ValueError) as error:
-        _get_map_individuals_to_track_ids(
-            list_individuals,
-            extract_track_id_from_individuals=True,
-        )
-
-    # Check that the error message is as expected
-    assert str(error.value) == expected_error_message
-
-
-@pytest.mark.parametrize(
-    "frame_max_digits",
-    [1, 100],
-    ids=["1_digit", "100_digits"],
-)
-@pytest.mark.parametrize(
-    "image_file_prefix, expected_prefix",
-    [
-        (None, ""),
-        ("", ""),
-        ("test_video", "test_video"),
-        ("test_video_", "test_video_"),
-    ],
-    ids=["no_prefix", "empty_prefix", "prefix", "prefix_underscore"],
-)
-@pytest.mark.parametrize(
-    "image_file_suffix, expected_suffix",
-    [
-        (".png", ".png"),
-        ("png", ".png"),
-        (".jpg", ".jpg"),
-    ],
-    ids=["png_extension", "png_no_dot", "jpg_extension"],
-)
-def test_get_image_filename_template(
-    frame_max_digits,
-    image_file_prefix,
-    expected_prefix,
-    image_file_suffix,
-    expected_suffix,
-):
-    """Test that the image filename template is as expected."""
-    expected_image_filename = (
-        f"{expected_prefix}{{:0{frame_max_digits}d}}{expected_suffix}"
-    )
-    assert (
-        save_bboxes._get_image_filename_template(
-            frame_max_digits=frame_max_digits,
-            image_file_prefix=image_file_prefix,
-            image_file_suffix=image_file_suffix,
-        )
-        == expected_image_filename
-    )
-
-
-@pytest.mark.parametrize(
     "valid_dataset",
     [
         "valid_bboxes_dataset",
@@ -329,3 +110,222 @@ def test_to_via_tracks_file_invalid_file_path(
             valid_bboxes_dataset,
             tmp_path / f"test{wrong_extension}",
         )
+
+
+@pytest.mark.parametrize(
+    "frame_max_digits",
+    [1, 100],
+    ids=["1_digit", "100_digits"],
+)
+@pytest.mark.parametrize(
+    "image_file_prefix, expected_prefix",
+    [
+        (None, ""),
+        ("", ""),
+        ("test_video", "test_video"),
+        ("test_video_", "test_video_"),
+    ],
+    ids=["no_prefix", "empty_prefix", "prefix", "prefix_underscore"],
+)
+@pytest.mark.parametrize(
+    "image_file_suffix, expected_suffix",
+    [
+        (".png", ".png"),
+        ("png", ".png"),
+        (".jpg", ".jpg"),
+    ],
+    ids=["png_extension", "png_no_dot", "jpg_extension"],
+)
+def test_get_image_filename_template(
+    frame_max_digits,
+    image_file_prefix,
+    expected_prefix,
+    image_file_suffix,
+    expected_suffix,
+):
+    """Test that the image filename template is as expected."""
+    expected_image_filename = (
+        f"{expected_prefix}{{:0{frame_max_digits}d}}{expected_suffix}"
+    )
+    assert (
+        save_bboxes._get_image_filename_template(
+            frame_max_digits=frame_max_digits,
+            image_file_prefix=image_file_prefix,
+            image_file_suffix=image_file_suffix,
+        )
+        == expected_image_filename
+    )
+
+
+@pytest.mark.parametrize(
+    "list_individuals, expected_track_id",
+    [
+        (["id_1", "id_3", "id_2"], [1, 3, 2]),
+        (["id_1", "id_2", "id_3"], [1, 2, 3]),
+        (["id-1", "id-2", "id-3"], [1, 2, 3]),
+        (["id1", "id2", "id3"], [1, 2, 3]),
+        (["id101", "id2", "id333"], [101, 2, 333]),
+        (["mouse_0_id1", "mouse_0_id2"], [1, 2]),
+    ],
+    ids=[
+        "unsorted",
+        "sorted",
+        "underscores",
+        "dashes",
+        "multiple_digits",
+        "middle_and_end_digits",
+    ],
+)
+def test_get_map_individuals_to_track_ids_from_individuals_names(
+    list_individuals, expected_track_id
+):
+    """Test the mapping individuals to track IDs if the track ID is
+    extracted from the individuals' names.
+    """
+    # Map individuals to track IDs
+    map_individual_to_track_id = _get_map_individuals_to_track_ids(
+        list_individuals, extract_track_id_from_individuals=True
+    )
+
+    # Check values are as expected
+    assert [
+        map_individual_to_track_id[individual]
+        for individual in list_individuals
+    ] == expected_track_id
+
+
+@pytest.mark.parametrize(
+    "list_individuals, expected_track_id",
+    [
+        (["A", "B", "C"], [0, 1, 2]),
+        (["C", "B", "A"], [2, 1, 0]),
+        (["id99", "id88", "id77"], [2, 1, 0]),
+    ],
+    ids=["sorted", "unsorted", "ignoring_digits"],
+)
+def test_get_map_individuals_to_track_ids_factorised(
+    list_individuals, expected_track_id
+):
+    """Test the mapping individuals to track IDs if the track ID is
+    factorised from the sorted individuals' names.
+    """
+    # Map individuals to track IDs
+    map_individual_to_track_id = _get_map_individuals_to_track_ids(
+        list_individuals, extract_track_id_from_individuals=False
+    )
+
+    # Check values are as expected
+    assert [
+        map_individual_to_track_id[individual]
+        for individual in list_individuals
+    ] == expected_track_id
+
+
+@pytest.mark.parametrize(
+    "list_individuals, expected_error_message",
+    [
+        (
+            ["mouse_1_id0", "mouse_2_id0"],
+            (
+                "Could not extract a unique track ID for all individuals. "
+                "Expected 2 unique track IDs, but got 1."
+            ),
+        ),
+        (
+            ["mouse_id1.0", "mouse_id2.0"],
+            (
+                "Could not extract a unique track ID for all individuals. "
+                "Expected 2 unique track IDs, but got 1."
+            ),
+        ),
+        (["A", "B", "C", "D"], "Could not extract track ID from A."),
+    ],
+    ids=["id_clash_1", "id_clash_2", "individuals_without_digits"],
+)
+def test_get_map_individuals_to_track_ids_error(
+    list_individuals, expected_error_message
+):
+    """Test that an error is raised if extracting track IDs from the
+    individuals' names fails.
+    """
+    with pytest.raises(ValueError) as error:
+        _get_map_individuals_to_track_ids(
+            list_individuals,
+            extract_track_id_from_individuals=True,
+        )
+
+    # Check that the error message is as expected
+    assert str(error.value) == expected_error_message
+
+
+@pytest.mark.parametrize(
+    "confidence",
+    [None, 0.5],
+    ids=["without_confidence", "with_confidence"],
+)
+@pytest.mark.parametrize(
+    "image_size",
+    [None, 100],
+    ids=["without_image_size", "with_image_size"],
+)
+@pytest.mark.parametrize(
+    "img_filename_template",
+    ["{:05d}.png", "{:03d}.jpg", "frame_{:03d}.jpg"],
+    ids=["png_extension", "jpg_extension", "frame_prefix"],
+)
+def test_write_single_row(
+    mock_csv_writer,
+    confidence,
+    image_size,
+    img_filename_template,
+):
+    """Test writing a single row of the VIA-tracks CSV file."""
+    # Fixed input values
+    frame, track_id, xy_coordinates, wh_values = (
+        1,
+        0,
+        np.array([100, 200]),
+        np.array([50, 30]),
+    )
+
+    # Write single row of VIA-tracks CSV file
+    with patch("csv.writer", return_value=mock_csv_writer):
+        row = _write_single_row(
+            mock_csv_writer,
+            xy_coordinates,
+            wh_values,
+            confidence,
+            track_id,
+            frame,
+            img_filename_template,
+            image_size,
+        )
+        mock_csv_writer.writerow.assert_called_with(row)
+
+    # Compute expected values
+    expected_filename = img_filename_template.format(frame)
+    expected_file_size = image_size if image_size is not None else 0
+    expected_file_attributes = "{}"  # placeholder value
+    expected_region_count = 0  # placeholder value
+    expected_region_id = 0  # placeholder value
+    expected_region_shape_attributes = {
+        "name": "rect",
+        "x": float(xy_coordinates[0] - wh_values[0] / 2),
+        "y": float(xy_coordinates[1] - wh_values[1] / 2),
+        "width": float(wh_values[0]),
+        "height": float(wh_values[1]),
+    }
+    expected_region_attributes = (
+        f'{{"track":"{int(track_id)}", "confidence":"{confidence}"}}'
+        if confidence is not None
+        else f'{{"track":"{int(track_id)}"}}'
+    )
+
+    # Check values are as expected
+    assert row[0] == expected_filename
+    assert row[1] == expected_file_size
+    assert row[2] == expected_file_attributes
+    assert row[3] == expected_region_count
+    assert row[4] == expected_region_id
+    assert row[5] == f"{expected_region_shape_attributes}"
+    assert row[6] == f"{expected_region_attributes}"
