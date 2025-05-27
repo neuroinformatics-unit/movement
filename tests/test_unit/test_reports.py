36c3c26542b41c3dcc630a79b616afbe195bdfb0
Chang Huan Lo <changhuan.lo@ucl.ac.uk>
2025-05-27 12:12:33 +0100

    Refactor `reports.py`  (#557)
    * Draft refactor
    
    * Make tests pass
    
    * Fix truncated df in nan report
    
    * Drop points without nans
    
    * Split tests
    
    * Resolve merge conflicts in test_kinematics.py
    
    * Add semicolon to end of nan report str
    
    * Update numpy dependency to require version 2.0.0 or higher

--- tests/test_unit/test_reports.py
+++ tests/test_unit/test_reports.py
@@ -1,110 +1,160 @@
+from contextlib import nullcontext as does_not_raise
+
+import numpy as np
 import pytest
+import xarray as xr
 
 from movement.utils.reports import report_nan_values
 
 
-@pytest.mark.parametrize(
-    "valid_dataset",
-    [
-        "valid_poses_dataset",
-        "valid_bboxes_dataset",
-        "valid_poses_dataset_with_nan",
-        "valid_bboxes_dataset_with_nan",
-    ],
-)
-@pytest.mark.parametrize(
-    "data_selection, expected_individuals_indices",
-    [
-        (lambda ds: ds.position, [0, 1]),  # full position data array
-        (
-            lambda ds: ds.position.isel(individuals=0),
-            [0],
-        ),  # individual 0 only
-    ],
-)
-def test_report_nan_values_in_position_selecting_individual(
-    valid_dataset,
-    data_selection,
-    expected_individuals_indices,
-    request,
-):
-    """Test that the nan-value reporting function handles position data
-    with specific ``individuals``, and that the data array name (position)
-    and only the relevant individuals are included in the report.
-    """
-    # extract relevant position data
-    input_dataset = request.getfixturevalue(valid_dataset)
-    output_data_array = data_selection(input_dataset)
-    # produce report
-    report_str = report_nan_values(output_data_array)
-    # check report of nan values includes name of data array
-    assert output_data_array.name in report_str
-    # check report of nan values includes selected individuals only
-    list_of_individuals = input_dataset["individuals"].values.tolist()
-    all_individuals = set(list_of_individuals)
-    expected_individuals = set(
-        list_of_individuals[i] for i in expected_individuals_indices
+def assert_components_in_report(components, report_str):
+    """Assert the expected components are in the report string."""
+    assert all(
+        component in report_str for component in components.get("expected", [])
+    ) and all(
+        component not in report_str
+        for component in components.get("not_expected", [])
     )
-    not_expected_individuals = all_individuals - expected_individuals
-    assert all(ind in report_str for ind in expected_individuals) and all(
-        ind not in report_str for ind in not_expected_individuals
-    ), "Report contains incorrect individuals."
 
 
 @pytest.mark.parametrize(
-    "valid_dataset",
-    ["valid_poses_dataset", "valid_poses_dataset_with_nan"],
-)
-@pytest.mark.parametrize(
-    "data_selection, expected_keypoints, expected_individuals",
+    "data, expectations",
     [
+        ("valid_poses_dataset", {"expected": ["No missing points"]}),
+        ("valid_bboxes_dataset", {"expected": ["No missing points"]}),
         (
-            lambda ds: ds.position,
-            {"centroid", "left", "right"},
-            {"id_0", "id_1"},
-        ),  # Report nans in position for all keypoints and individuals
+            "valid_poses_dataset_with_nan",
+            {
+                "expected": [
+                    "position",
+                    "keypoints",
+                    "centroid",
+                    "left",
+                    "right",
+                    "individuals",
+                    "id_0",
+                    "3/10",
+                    "1/10",
+                    "10/10",
+                ],
+                "not_expected": ["id_1"],
+            },
+        ),
         (
-            lambda ds: ds.position.sel(keypoints=["centroid", "left"]),
-            {"centroid", "left"},
-            {"id_0", "id_1"},
-        ),  # Report nans in position for 2 keypoints, for all individuals
-        (
-            lambda ds: ds.position.sel(
-                individuals="id_0", keypoints="centroid"
-            ),
-            set(),
-            {"id_0"},
-        ),  # Report nans in position for centroid of individual id_0
-        # Note: if only 1 keypoint exists, its name is not explicitly reported
+            "valid_bboxes_dataset_with_nan",
+            {
+                "expected": ["position", "individuals", "id_0", "3/10"],
+                "not_expected": ["id_1"],
+            },
+        ),
     ],
 )
-def test_report_nan_values_in_position_selecting_keypoint(
-    valid_dataset,
-    data_selection,
-    expected_keypoints,
-    expected_individuals,
-    request,
-):
-    """Test that the nan-value reporting function handles position data
-    with specific ``keypoints`` , and that the data array name (position)
-    and only the relevant keypoints are included in the report.
+def test_report_nan_values_full_dataset(data, expectations, request):
+    """Test that the nan-value reporting function handles full and
+    valid data with or without NaN values and that the report contains
+    the correct NaN counts, keypoints, and individuals.
     """
-    # extract relevant position data
-    input_dataset = request.getfixturevalue(valid_dataset)
-    output_data_array = data_selection(input_dataset)
-    # produce report
-    report_str = report_nan_values(output_data_array)
-    # check report of nan values includes name of data array
-    assert output_data_array.name in report_str
-    # check report of nan values includes only selected keypoints
-    all_keypoints = set(input_dataset["keypoints"].values.tolist())
-    not_expected_keypoints = all_keypoints - expected_keypoints
-    assert all(kpt in report_str for kpt in expected_keypoints) and all(
-        kpt not in report_str for kpt in not_expected_keypoints
-    ), "Report contains incorrect keypoints."
-    # check report of nan values includes selected individuals only
-    all_individuals = set(input_dataset["individuals"].values.tolist())
-    not_expected_individuals = all_individuals - expected_individuals
-    assert all(ind in report_str for ind in expected_individuals) and all(
-        ind not in report_str for ind in not_expected_individuals
-    ), "Report contains incorrect individuals."
+    da = request.getfixturevalue(data).position
+    report_str = report_nan_values(da)
+    assert_components_in_report(expectations, report_str)
+
+
+@pytest.mark.parametrize(
+    "data, selection_fn, expectations",
+    [
+        (
+            "valid_poses_dataset_with_nan",
+            lambda ds: ds.isel(individuals=0),
+            {
+                "expected": [
+                    "centroid",
+                    "left",
+                    "right",
+                    "3/10",
+                    "1/10",
+                    "10/10",
+                ],
+                "not_expected": ["id_0"],
+            },
+        ),
+        (
+            "valid_bboxes_dataset_with_nan",
+            lambda ds: ds.isel(individuals=0),
+            {
+                "expected": ["3/10"],
+                "not_expected": ["id_0"],
+            },
+        ),
+        (
+            "valid_poses_dataset_with_nan",
+            lambda ds: ds.isel(keypoints=0),
+            {
+                "expected": ["id_0", "3/10"],
+                "not_expected": ["centroid"],
+            },
+        ),
+        (
+            "valid_poses_dataset_with_nan",
+            lambda ds: ds.isel(individuals=0, keypoints=0),
+            {
+                "expected": ["3/10"],
+                "not_expected": ["centroid", "id_0"],
+            },
+        ),
+    ],
+    ids=[
+        "ind_dim_with_ndim_0-poses",  # individuals dim is scalar
+        "ind_dim_with_ndim_0-bboxes",  # individuals dim is scalar
+        "kp_dim_with_ndim_0-poses",  # keypoints dim is scalar
+        "both_dims_with_ndim_0-poses",  # both dims are scalar
+    ],
+)  # If ndim=0, the dim coords are not explicitly reported
+def test_report_nan_values_scalar_dims(
+    data, selection_fn, expectations, request
+):
+    """Test that the nan-value reporting function handles data with
+    scalar dimensions (i.e. dimension.ndim == 0), for example, when
+    using ``isel()`` or ``sel()`` to select a single individual or
+    keypoint.
+    """
+    da = selection_fn(request.getfixturevalue(data)).position
+    report_str = report_nan_values(da)
+    assert_components_in_report(expectations, report_str)
+
+
+@pytest.mark.parametrize(
+    "data, fetch_data, expectations",
+    [
+        (
+            "valid_poses_dataset_with_nan",
+            lambda ds: ds.rename({"space": "other"}),
+            does_not_raise({"expected": ["position", "other", "x", "y"]}),
+        ),  # count NaNs separately for x and y
+        (
+            "simple_data_array_with_nan",
+            lambda: xr.DataArray([1, np.nan, 3], dims="time"),
+            does_not_raise({"expected": ["data", "1/3"]}),
+        ),  # generic data array with required time dim
+        (
+            "invalid_data_array_with_nan",
+            lambda: xr.DataArray([1, np.nan, 3], dims="dim1"),
+            pytest.raises(ValueError, match=".*must contain.*time.*"),
+        ),  # invalid data array without required time dim
+    ],
+    ids=["separate_x_y_dims", "simple_data_array", "missing_time_dim"],
+)
+def test_report_nan_values_arbitrary_dims(
+    data, fetch_data, expectations, request
+):
+    """Test that the nan-value reporting function handles data with
+    arbitrary dimensions as long as the required `time` dimension is
+    present.
+    """
+    da = (
+        fetch_data(request.getfixturevalue(data).position)
+        if data == "valid_poses_dataset_with_nan"
+        else fetch_data()
+    )
+    with expectations as e:
+        report_str = report_nan_values(da)
+        assert_components_in_report(e, report_str)
