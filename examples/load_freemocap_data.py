986de3a83b150821d1e274a3235c83c70a4180ac
Max Staras <50526197+maxstaras@users.noreply.github.com>
2025-07-14 21:51:31 +0100

    reverted to prior read_freemocap_as_ds, added body visualisation

--- examples/load_freemocap_data.py
+++ examples/load_freemocap_data.py
@@ -7,10 +7,10 @@ Load the ``.csv`` files from FreeMoCap into movement and visualise them.
 # %%
 # Imports
 # -------
-import re
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -70,32 +70,28 @@ recording_dir_world = session_dir_path.joinpath(
 # ``.csv`` files into one ``movement`` dataset.
 def read_freemocap_as_ds(recording_dir_path, individual_name="person_0"):
     model = ["body", "left_hand", "right_hand", "face"]
-    ds_unified = xr.Dataset()
+    full_data = xr.Dataset()
     for c in model:
-        df = pd.read_csv(
+        data_pd = pd.read_csv(
             recording_dir_path.joinpath(f"mediapipe_{c}_3d_xyz.csv")
         )
-        list_columns = list(df.columns)
-        data = df.to_numpy()
-        # Split data array
-        data_split = np.split(data, int(data.shape[1] / 3), axis=1)
-        # returns a list of (M,3) arrays, one per keypoint
-
-        # Stack arrays along new "keypoint" dimension to get the position array
-        position_array = np.stack(data_split, axis=-1)[:, :, :, None]
-        # (n_frames, 3, n_keypoints, 1), for 3D coordinates and one individual
-
+        headers = data_pd.columns
+        data = data_pd.to_numpy()
+        data = np.array(
+            [data.reshape(data.shape[0], int(data.shape[1] / 3), 3)]
+        )
+        # Transpose to align with movement data shape
+        data_transposed = np.transpose(data, [1, 3, 2, 0])
         ds = load_poses.from_numpy(
-            position_array,
+            position_array=data_transposed,
+            confidence_array=np.ones(np.delete(data_transposed.shape, 1)),
             individual_names=[individual_name],
             # Select and trim header names
-            keypoint_names=set(
-                [re.sub(r"_[xyz]$", "", col) for col in list_columns]
-            ),
+            keypoint_names=[a[:-2] for a in headers[::3]],
         )
         # Merge all of the Datasets into one, along the keypoints axis
-        ds_unified = ds_unified.merge(ds, 2)
-    return ds_unified
+        full_data = full_data.merge(ds, 2)
+    return full_data
 
 
 ds_hello = read_freemocap_as_ds(recording_dir_hello)
@@ -145,14 +141,131 @@ def coloured_scatter_line_3d(x, y, z, c, s, ax, **lc_kwargs):
     return lc
 
 
-# Plot A: Frame
-# -------------
-# ``x, y, z`` where time determines colour.
+# %%
+# Visualising the skeleton
+# ------------------------
+# Using
+# `NetworkX <https://networkx.org/>`_
+# to join some keypoints and construct a skeleton
+# of the upper-half of the individual.
+
+# Select some keypoints from the `body` model
+selected_body_kpts = [
+    "body_nose",
+    "body_left_eye",
+    "body_right_eye",
+    "body_left_shoulder",
+    "body_right_shoulder",
+    "body_left_hip",
+    "body_right_hip",
+    "body_left_elbow",
+    "body_right_elbow",
+    "body_left_wrist",
+    "body_right_wrist",
+    "body_left_index",
+    "body_right_index",
+    "body_left_pinky",
+    "body_right_pinky",
+    "body_left_thumb",
+    "body_right_thumb",
+    "body_left_ear",
+    "body_right_ear",
+    "body_mouth_left",
+    "body_mouth_right",
+]
+
+# Choose a frame index to create the body from
+body_frame = 130
+
+# Initialize graph
+G = nx.Graph()
+
+# Add nodes
+for kpt in selected_body_kpts:
+    G.add_node(
+        kpt,
+        position=ds_hello.position.sel(
+            time=body_frame, keypoints=kpt
+        ).values,  # (n_frames, 3)
+    )
+
+# Add midpoint between the shoulders as node
+G.add_node(
+    "body_shoulders_midpoint",
+    position=np.mean(
+        ds_hello.position.sel(
+            time=body_frame,
+            keypoints=["body_left_shoulder", "body_right_shoulder"],
+        ).values,
+        axis=1,
+    ),
+)
+
+# Add edges
+G.add_edge("body_right_shoulder", "body_left_shoulder")
+for side_str in ["left", "right"]:
+    G.add_edge(f"body_{side_str}_shoulder", f"body_{side_str}_elbow")
+    G.add_edge(f"body_{side_str}_shoulder", f"body_{side_str}_hip")
+    G.add_edge(f"body_{side_str}_elbow", f"body_{side_str}_wrist")
+    G.add_edge(f"body_{side_str}_wrist", f"body_{side_str}_index")
+    G.add_edge(f"body_{side_str}_wrist", f"body_{side_str}_pinky")
+    G.add_edge(f"body_{side_str}_wrist", f"body_{side_str}_thumb")
+    G.add_edge(f"body_mouth_{side_str}", f"body_{side_str}_ear")
+
+# Add edge between the shoulders midpoint and the nose
+G.add_edge("body_shoulders_midpoint", "body_nose")
+
+# Add edge between the ears
+G.add_edge("body_left_ear", "body_right_ear")
+
+# Add edge across the mouth
+G.add_edge("body_mouth_left", "body_mouth_right")
+
 fig_a = plt.figure()
-axes_a = fig_a.add_subplot(projection="3d")
+ax_a = fig_a.add_subplot(111, projection="3d")
 
-colour_map_a = "turbo"
+# Get positions dictionary (key: node, value: (x, y, z))
+positions_dict = nx.get_node_attributes(G, "position")
 
+# Plot nodes
+for _node, (x, y, z) in positions_dict.items():
+    ax_a.scatter(x, y, z, s=10, color="green")
+
+# Plot edges
+for edge in G.edges():
+    node_1, node_2 = edge
+    x1, y1, z1 = positions_dict[node_1]
+    x2, y2, z2 = positions_dict[node_2]
+    ax_a.plot([x1, x2], [y1, y2], [z1, z2], "b-")
+
+# Plot the text keypoints for all frames
+ax_a.plot(
+    position_hello.sel(space="x"),
+    position_hello.sel(space="y"),
+    position_hello.sel(space="z"),
+    alpha=0.35,
+    color="magenta",
+)
+
+ax_a.view_init(elev=25, azim=-120, roll=0)
+ax_a.set_aspect("equal")
+ax_a.set_xlabel("x (mm, inverted)")
+ax_a.set_ylabel("y (mm)")
+ax_a.set_zlabel("z (mm)")
+
+# Invert ``x-axis`` to make traced text readable
+ax_a.invert_xaxis()
+
+
+# %%
+# Displaying speed and time as colours
+# ------------------------------------
+# Visualising the frame-index
+# ``x, y, z`` where time determines colour.
+fig_b = plt.figure()
+ax_b = fig_b.add_subplot(projection="3d")
+
+colour_map_b = "turbo"
 # Use time component of Dataset
 frame_hello = ds_hello.time
 frame_world = ds_world.time
@@ -163,10 +276,10 @@ coloured_scatter_line_3d(
     position_hello.sel(space="x"),
     position_hello.sel(space="y"),
     position_hello.sel(space="z"),
-    ax=axes_a,
+    ax=ax_b,
     c=frame_hello,
     s=5,
-    cmap=colour_map_a,
+    cmap=colour_map_b,
 )
 # Add "world" scatter and line
 
@@ -174,22 +287,26 @@ coloured_scatter_line_3d(
     position_world.sel(space="x"),
     position_world.sel(space="y"),
     position_world.sel(space="z") - 400,
-    ax=axes_a,
+    ax=ax_b,
     c=frame_world,
     s=5,
-    cmap=colour_map_a,
+    cmap=colour_map_b,
 )
 
 # Change view orientation
-axes_a.view_init(elev=-20, azim=137, roll=0)
+ax_b.view_init(elev=-20, azim=137, roll=0)
+ax_b.set_aspect("equal")
+ax_b.set_xlabel("x (mm)")
+ax_b.set_ylabel("y (mm)")
+ax_b.set_zlabel("z (mm)")
+ax_b.set_title("Visualising frame-index", pad=40)
 
-# Plot B: Speed
-# -------------
+# Next, visualising the current writing speed
 # ``x, y, z`` where speed determines colour.
-fig_b = plt.figure()
-axes_b = fig_b.add_subplot(projection="3d")
+fig_c = plt.figure()
+ax_c = fig_c.add_subplot(projection="3d")
 
-colour_map_b = "inferno"
+colour_map_c = "inferno"
 
 # Use movement helpers to compute speed
 speed_hello = compute_speed(position_hello)
@@ -200,24 +317,27 @@ coloured_scatter_line_3d(
     position_hello.sel(space="x"),
     position_hello.sel(space="y"),
     position_hello.sel(space="z"),
-    ax=axes_b,
+    ax=ax_c,
     c=speed_hello,
     s=5,
-    cmap=colour_map_b,
+    cmap=colour_map_c,
 )
 # Add "world" scatter and line
 coloured_scatter_line_3d(
     position_world.sel(space="x"),
     position_world.sel(space="y"),
     position_world.sel(space="z") - 400,
-    ax=axes_b,
+    ax=ax_c,
     c=speed_world,
     s=5,
-    cmap=colour_map_b,
+    cmap=colour_map_c,
 )
 
 # Change view orientation
-axes_b.view_init(elev=-20, azim=137, roll=0)
-
-
+ax_c.view_init(elev=-20, azim=137, roll=0)
+ax_c.set_aspect("equal")
+ax_c.set_xlabel("x (mm)")
+ax_c.set_ylabel("y (mm)")
+ax_c.set_zlabel("z (mm)")
+ax_c.set_title("Visualising writing speed", pad=40)
 # %%
