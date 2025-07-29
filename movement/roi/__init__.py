3feacc5a195963694ad1d73235781fbfa913184b
lochhh <changhuan.lo@ucl.ac.uk>
2025-07-29 17:39:12 +0100

    Define `__all__` for `movement.roi`

--- movement/roi/__init__.py
+++ movement/roi/__init__.py
@@ -1,3 +1,9 @@
 from movement.roi.conditions import compute_region_occupancy
 from movement.roi.line import LineOfInterest
 from movement.roi.polygon import PolygonOfInterest
+
+__all__ = [
+    "compute_region_occupancy",
+    "LineOfInterest",
+    "PolygonOfInterest",
+]
