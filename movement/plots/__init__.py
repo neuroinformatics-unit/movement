ac087b39be6e8f0d609583a884b2846bb6667c9a
lochhh <changhuan.lo@ucl.ac.uk>
2025-07-30 12:18:41 +0100

    Update module descriptions

--- movement/plots/__init__.py
+++ movement/plots/__init__.py
@@ -1,3 +1,5 @@
+"""Plotting utilities for ``movement`` datasets."""
+
 from movement.plots.occupancy import plot_occupancy
 from movement.plots.trajectory import plot_centroid_trajectory
 
--- movement/roi/__init__.py
+++ movement/roi/__init__.py
@@ -1,3 +1,5 @@
+"""Utilities for representing and analysing regions of interest."""
+
 from movement.roi.conditions import compute_region_occupancy
 from movement.roi.line import LineOfInterest
 from movement.roi.polygon import PolygonOfInterest
