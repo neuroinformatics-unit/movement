487c9604c3dc823bcc0cd8af04b6ad50c16ef2f8
lochhh <changhuan.lo@ucl.ac.uk>
2025-07-31 15:02:18 +0100

    Add BaseRegionOfInterest to `roi` module exports

--- movement/roi/__init__.py
+++ movement/roi/__init__.py
@@ -1,5 +1,6 @@
 """Utilities for representing and analysing regions of interest."""
 
+from movement.roi.base import BaseRegionOfInterest
 from movement.roi.conditions import compute_region_occupancy
 from movement.roi.line import LineOfInterest
 from movement.roi.polygon import PolygonOfInterest
@@ -8,4 +9,5 @@ __all__ = [
     "compute_region_occupancy",
     "LineOfInterest",
     "PolygonOfInterest",
+    "BaseRegionOfInterest",
 ]
