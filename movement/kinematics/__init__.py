8a8502efcda80c0c5ddafb9e8c84c5efa5009c14
Carlo Castoldi <carlo.castoldi@outlook.com>
2025-08-15 12:02:40 +0100

    Improve displace vectors computation
    * remove movement.kinematics.compute_displacement
    * add movement.kinematics.compute_forward_displacement
    * add movement.kinematics.compute_backward_displacement
    * adapt examples and docstrings accordingly
    * adapt tests to the implementation changes

--- movement/kinematics/__init__.py
+++ movement/kinematics/__init__.py
@@ -3,7 +3,8 @@
 from movement.kinematics.distances import compute_pairwise_distances
 from movement.kinematics.kinematics import (
     compute_acceleration,
-    compute_displacement,
+    compute_forward_displacement,
+    compute_backward_displacement,
     compute_path_length,
     compute_speed,
     compute_time_derivative,
@@ -17,7 +18,8 @@ from movement.kinematics.orientation import (
 from movement.kinematics.kinetic_energy import compute_kinetic_energy
 
 __all__ = [
-    "compute_displacement",
+    "compute_forward_displacement",
+    "compute_backward_displacement",
     "compute_velocity",
     "compute_acceleration",
     "compute_speed",
