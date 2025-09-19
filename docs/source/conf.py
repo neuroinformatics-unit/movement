28f8dd1de6918a42a550f0e89795855245a274e1
sfmig <33267254+sfmig@users.noreply.github.com>
2025-09-19 13:11:39 +0100

    Add robots.ox.ac.uk to linkcheck_ignore

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -190,6 +190,7 @@ linkcheck_ignore = [
     "https://pubs.acs.org/doi/*",  # Checking dois is forbidden here
     "https://opensource.org/license/bsd-3-clause/",  # to avoid odd 403 error
     "https://www.sainsburywellcome.org/",  # Occasional ConnectTimeoutError
+    "https://www.robots.ox.ac.uk/",  # occasional 404s
 ]
 
 
