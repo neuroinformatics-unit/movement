91f3ef2ceec05be39e68df0f9301c93bfdb5c6db
niksirbi <niko.sirbiladze@gmail.com>
2026-01-08 17:21:19 +0000

    ignore all contributor covenant links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -209,7 +209,7 @@ linkcheck_ignore = [
     "https://www.robots.ox.ac.uk/",  # occasional 404s
     "https://silvalab.codeberg.page/BraiAn/",  # SSLError despite working link
     "https://www.g-node.org/",  # frequent timeouts
-    "https://www.contributor-covenant.org/resources",  # frequent timeouts
+    "https://www.contributor-covenant.org/*",  # frequent timeouts
     # Checking zenodo redirects (from concept doi to record) takes a long time
     "https://zenodo.org/doi/*",
     "https://zenodo.org/records/*",
