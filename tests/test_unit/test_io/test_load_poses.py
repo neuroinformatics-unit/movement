d6cda1ee240a4d14f4b17fa918e5e54b41b01ecd
lochhh <changhuan.lo@ucl.ac.uk>
2026-02-11 17:45:18 +0000

    Restore `from_file` and `from_multiview_files` tests

--- tests/test_unit/test_io/test_load_poses.py
+++ tests/test_unit/test_io/test_load_poses.py
@@ -1,5 +1,7 @@
 """Test suite for the load_poses module."""
 
+from unittest.mock import patch
+
 import numpy as np
 import pytest
 import xarray as xr
@@ -268,3 +270,52 @@ def test_load_from_nwb_file(input_type, kwargs, request):
     if input_type == "nwb_file":
         expected_attrs["source_file"] = nwb_file
     assert ds_from_file_path.attrs == expected_attrs
+
+
+@pytest.mark.parametrize(
+    "source_software",
+    ["DeepLabCut", "SLEAP", "LightningPose", "Anipose", "NWB", "Unknown"],
+)
+@pytest.mark.parametrize("fps", [None, 30, 60.0])
+def test_from_file_delegates_correctly(source_software, fps, caplog):
+    """Test that the from_file() function delegates to the correct
+    loader function according to the source_software.
+    """
+    software_to_loader = {
+        "DeepLabCut": "movement.io.load_poses.from_dlc_file",
+        "SLEAP": "movement.io.load_poses.from_sleap_file",
+        "LightningPose": "movement.io.load_poses.from_lp_file",
+        "Anipose": "movement.io.load_poses.from_anipose_file",
+        "NWB": "movement.io.load_poses.from_nwb_file",
+    }
+    if source_software == "Unknown":
+        with pytest.raises(ValueError, match="Unsupported source"):
+            load_poses.from_file("some_file", source_software)
+    else:
+        with patch(software_to_loader[source_software]) as mock_loader:
+            load_poses.from_file("some_file", source_software, fps)
+            expected_call_args = (
+                ("some_file", fps)
+                if source_software != "NWB"
+                else ("some_file",)
+            )
+            mock_loader.assert_called_with(*expected_call_args)
+            if source_software == "NWB" and fps is not None:
+                assert "fps argument is ignored" in caplog.messages[0]
+
+
+def test_from_multiview_files():
+    """Test loading pose tracks from multiple files (representing
+    different views).
+    """
+    view_names = ["view_0", "view_1"]
+    file_path_dict = {
+        view: DATA_PATHS.get("DLC_single-wasp.predictions.h5")
+        for view in view_names
+    }
+    multi_view_ds = load_poses.from_multiview_files(
+        file_path_dict, source_software="DeepLabCut"
+    )
+    assert isinstance(multi_view_ds, xr.Dataset)
+    assert "view" in multi_view_ds.dims
+    assert multi_view_ds.view.values.tolist() == view_names
