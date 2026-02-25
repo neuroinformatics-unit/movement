6059df4577901280c1673a0333219c0eac85ab2c
Hemant yadav data scientist <hemantyaduvanshi585@gmail.com>
2026-02-26 02:14:14 +0530

    Refactor: sort __all__ in kinematics init alphabetically
    Sorted the __all__ list to follow Python best practices for maintainability.

--- movement/kinematics/__init__.py
+++ movement/kinematics/__init__.py
@@ -19,17 +19,17 @@ from movement.kinematics.orientation import (
 from movement.kinematics.kinetic_energy import compute_kinetic_energy
 
 __all__ = [
-    "compute_displacement",
-    "compute_forward_displacement",
-    "compute_backward_displacement",
-    "compute_velocity",
-    "compute_acceleration",
-    "compute_speed",
-    "compute_path_length",
-    "compute_time_derivative",
-    "compute_pairwise_distances",
-    "compute_forward_vector",
-    "compute_head_direction_vector",
-    "compute_forward_vector_angle",
-    "compute_kinetic_energy",
+    "compute_acceleration",            # A
+    "compute_backward_displacement",   # B
+    "compute_displacement",            # D
+    "compute_forward_displacement",    # F
+    "compute_forward_vector",          # F (v)
+    "compute_forward_vector_angle",    # F (va)
+    "compute_head_direction_vector",   # H
+    "compute_kinetic_energy",          # K
+    "compute_path_length",             # P
+    "compute_pairwise_distances",      # P (ai)
+    "compute_speed",                   # S
+    "compute_time_derivative",         # T
+    "compute_velocity",                # V
 ]
