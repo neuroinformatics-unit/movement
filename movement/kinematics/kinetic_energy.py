bc21671a370c032fa392d35246f0e0cd08c12d9a
karinaantoniu <karina.antoniu@gmail.com>
2026-02-27 12:12:36 +0200

    FEAT: Handle NaN in kinetic energy and add warning threshold

--- movement/kinematics/kinetic_energy.py
+++ movement/kinematics/kinetic_energy.py
@@ -7,6 +7,7 @@ from movement.kinematics.kinematics import compute_velocity
 from movement.utils.logging import logger
 from movement.utils.vector import compute_norm
 from movement.validators.arrays import validate_dims_coords
+from movement.kinematics.kinematics import _warn_about_nan_proportion
 
 
 def compute_kinetic_energy(
@@ -14,6 +15,7 @@ def compute_kinetic_energy(
     keypoints: list | None = None,
     masses: dict | None = None,
     decompose: bool = False,
+    nan_warn_threshold: float = 0.1, # how much missing data is acceptable before a warning
 ) -> xr.DataArray:
     r"""Compute kinetic energy per individual.
 
@@ -38,6 +40,11 @@ def compute_kinetic_energy(
         per individual, but more would be desirable for a meaningful
         decomposition. The default is False, meaning the total kinetic energy
         is returned.
+    nan_warn_threshold: float, optional
+		If some keypoints are missing at some point in time, kinetic energy
+        is computed only on the remaining valid points. If any point has at least
+        threshold values missing, a warning will be emitted.
+        By default 0.1.
 
     Returns
     -------
@@ -142,6 +149,7 @@ def compute_kinetic_energy(
     validate_dims_coords(
         position, {"time": [], "space": ["x", "y"], "keypoints": []}
     )
+    _warn_about_nan_proportion(position, nan_warn_threshold)
 
     # Subset keypoints if requested
     if keypoints is not None:
@@ -173,7 +181,7 @@ def compute_kinetic_energy(
 
     # Compute total KE
     weighted_ke = 0.5 * weights * (compute_norm(velocity) ** 2)
-    ke_total = weighted_ke.sum(dim="keypoints")
+    ke_total = weighted_ke.sum(dim="keypoints", min_count=1)
 
     if not decompose:
         return ke_total
