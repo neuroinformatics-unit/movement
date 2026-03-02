96bf0f463065824cce5ff299e0c4a695ae1871de
niksirbi <niko.sirbiladze@gmail.com>
2026-03-02 15:51:24 +0000

    remove internal type from __init__py

--- movement/roi/__init__.py
+++ movement/roi/__init__.py
@@ -2,7 +2,7 @@
 
 from movement.roi.base import BaseRegionOfInterest
 from movement.roi.conditions import compute_region_occupancy
-from movement.roi.io import load_rois, save_rois, ROICollection
+from movement.roi.io import load_rois, save_rois
 from movement.roi.line import LineOfInterest
 from movement.roi.polygon import PolygonOfInterest
 
@@ -10,7 +10,6 @@ __all__ = [
     "BaseRegionOfInterest",
     "LineOfInterest",
     "PolygonOfInterest",
-    "ROICollection",
     "compute_region_occupancy",
     "load_rois",
     "save_rois",
