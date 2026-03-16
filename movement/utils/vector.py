7ec16295c1ea90280a5e61849d1a2cc67ecf5680
PewterZz <peterns1609@gmail.com>
2026-03-16 18:04:29 +0700

    fix: raise errors correctly in vector.py instead of raise logger.error()

--- movement/utils/vector.py
+++ movement/utils/vector.py
@@ -268,21 +268,21 @@ def compute_signed_angle_2d(
         elif v.ndim == 2:
             v_dims = ["time", "space"]
         else:
-            raise logger.error(
-                ValueError(f"v must be 1D or 2D, but got {v.ndim}D.")
-            )
+            error = ValueError(f"v must be 1D or 2D, but got {v.ndim}D.")
+            logger.error(error)
+            raise error
         v = xr.DataArray(
             v,
             dims=v_dims,
             coords={d: u.coords[d] for d in v_dims},
         )
     elif not isinstance(v, xr.DataArray):
-        raise logger.error(
-            TypeError(
-                "v must be an xarray.DataArray or np.ndarray, "
-                f"but got {type(v)}."
-            )
+        error = TypeError(
+            "v must be an xarray.DataArray or np.ndarray, "
+            f"but got {type(v)}."
         )
+        logger.error(error)
+        raise error
     validate_dims_coords(v, {"space": ["x", "y"]}, exact_coords=True)
 
     u = convert_to_unit(u)
@@ -308,9 +308,9 @@ def compute_signed_angle_2d(
 
 
 def _raise_error_for_missing_spatial_dim() -> NoReturn:
-    raise logger.error(
-        ValueError(
-            "Input data array must contain either 'space' or 'space_pol' "
-            "as dimensions."
-        )
+    error = ValueError(
+        "Input data array must contain either 'space' or 'space_pol' "
+        "as dimensions."
     )
+    logger.error(error)
+    raise error
