2d9d42f9d74815d94f638269984ebdf9fa2dfa73
anna-teruel <teruel.anna@gmail.com>
2026-08-03 13:45:22 +0200

    adding edited widget to display an edit slidebar

--- movement/napari/meta_widget.py
+++ movement/napari/meta_widget.py
@@ -3,6 +3,7 @@
 from napari.viewer import Viewer
 from qt_niu.collapsible_widget import CollapsibleWidgetContainer
 
+from movement.napari.edit_widget import EditWidget
 from movement.napari.loader_widgets import DataLoader
 from movement.napari.regions_widget import RegionsWidget
 from movement.napari.save_widget import DataSaver
@@ -42,3 +43,12 @@ class MovementMetaWidget(CollapsibleWidgetContainer):
 
         loader_collapsible = self.collapsible_widgets[0]
         loader_collapsible.expand()  # expand the loader widget by default
+
+        # The edit widget is a timeline, best shown full-width rather
+        # than squeezed into this side panel, so it's docked separately
+        # at the bottom instead of being added above as a collapsible
+        # section.
+        self.edit_widget = EditWidget(napari_viewer)
+        napari_viewer.window.add_dock_widget(
+            self.edit_widget, area="bottom", name="edited frames"
+        )
